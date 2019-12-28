6626869312168f0fe28012b1721c9c349fc77e8d
Yunguan Fu <fuyunguan@gmail.com>
2019-12-28 16:58:51 +0000

    clean code

--- src/model/network.py
+++ src/model/network.py
@@ -1,85 +1,86 @@
 import tensorflow as tf
 
 import src.model.layer as layer
-import src.model.util as util
+import src.model.layer_util as layer_util
+import src.model.loss as loss
 
 
 class LocalModel(tf.keras.Model):
-    def __init__(self, batch_size, num_channel_initial, ddf_levels=None, **kwargs):
+    def __init__(self,
+                 moving_image_size, fixed_image_size,
+                 batch_size, num_channel_initial, ddf_levels=None, **kwargs):
         super(LocalModel, self).__init__(**kwargs)
 
         # save parameters
-        self._batch_size = batch_size
-        self._ddf_levels = [0, 1, 2, 3, 4] if ddf_levels is None else ddf_levels
-        self._moving_image_size = None
-        self._fixed_image_size = None
-
-        # init layer variables
-        self._resize3d = None
-
-        nc = [num_channel_initial * (2 ** i) for i in range(5)]
-        self._downsample_resnet_block0 = layer.DownSampleResnetBlock(filters=nc[0], kernel_size=7)
-        self._downsample_resnet_block1 = layer.DownSampleResnetBlock(filters=nc[1])
-        self._downsample_resnet_block2 = layer.DownSampleResnetBlock(filters=nc[2])
-        self._downsample_resnet_block3 = layer.DownSampleResnetBlock(filters=nc[3])
-        self._conv3d_block4 = layer.Conv3dBlock(filters=nc[4])
-
-        upsample_resnet_blocks = []
-        min_level = min(self._ddf_levels)
-        for i in range(4, 0, -1):
-            if min_level < i:
-                upsample_resnet_blocks.append(layer.UpSampleResnetBlock(filters=nc[i - 1]))
-        self._upsample_resnet_blocks = upsample_resnet_blocks
-        self._ddf_summands = None
-        self._grid_ref = None
-
-    def build(self, input_shape):
-        # sanity check
-        if not (isinstance(input_shape, list) or isinstance(input_shape, tuple)):
-            raise ValueError("LocalModel's inputs are [moving_image, fixed_image, moving_label]")
-        if len(input_shape) != 3:
-            raise ValueError("LocalModel's inputs are [moving_image, fixed_image, moving_label]")
-        super(LocalModel, self).build(input_shape)
-
-        moving_image_size = input_shape[0][1:4]
-        fixed_image_size = input_shape[1][1:4]
         self._moving_image_size = moving_image_size
         self._fixed_image_size = fixed_image_size
+        self._batch_size = batch_size
+        self._ddf_levels = [0, 1, 2, 3, 4] if ddf_levels is None else ddf_levels
+        self._ddf_min_level = min(self._ddf_levels)
+
+        # init layer variables
         self._resize3d = layer.Resize3d(size=fixed_image_size)
+
+        nc = [num_channel_initial * (2 ** i) for i in range(5)]
+        self._downsample_block0 = layer.DownSampleResnetBlock(filters=nc[0], kernel_size=7)
+        self._downsample_block1 = layer.DownSampleResnetBlock(filters=nc[1])
+        self._downsample_block2 = layer.DownSampleResnetBlock(filters=nc[2])
+        self._downsample_block3 = layer.DownSampleResnetBlock(filters=nc[3])
+        self._conv3d_block4 = layer.Conv3dBlock(filters=nc[4])
+
+        self._upsample_block3 = layer.UpSampleResnetBlock(nc[3]) if self._ddf_min_level < 4 else None
+        self._upsample_block2 = layer.UpSampleResnetBlock(nc[2]) if self._ddf_min_level < 3 else None
+        self._upsample_block1 = layer.UpSampleResnetBlock(nc[1]) if self._ddf_min_level < 2 else None
+        self._upsample_block0 = layer.UpSampleResnetBlock(nc[0]) if self._ddf_min_level < 1 else None
+
         self._ddf_summands = [layer.DDFSummand(output_shape=fixed_image_size) for _ in self._ddf_levels]
-        self._grid_ref = util.get_reference_grid(grid_size=fixed_image_size)
 
     def call(self, inputs, training=None, mask=None):
-        # sanity check
-        if not isinstance(inputs, list):
-            raise ValueError("LocalModel's inputs are [moving_image, fixed_image, moving_label]")
-        if len(inputs) != 3:
-            raise ValueError("LocalModel's inputs are [moving_image, fixed_image, moving_label]")
+        layer_util.check_inputs(inputs, 2, "LocalModel")
 
-        moving_image, fixed_image, moving_label = inputs[0], inputs[1], inputs[2]
-
-        moving_image = tf.reshape(moving_image, [self._batch_size] + self._moving_image_size + [1])
-        fixed_image = tf.reshape(fixed_image, [self._batch_size] + self._fixed_image_size + [1])
-        moving_label = tf.reshape(moving_label, [self._batch_size] + self._moving_image_size + [1])
-        inputs = tf.concat([self._resize3d(inputs=moving_image), fixed_image],
+        moving_image, fixed_image = inputs[0], inputs[1]
+        moving_image = tf.expand_dims(moving_image, axis=4)
+        fixed_image = tf.expand_dims(fixed_image, axis=4)
+        images = tf.concat([self._resize3d(inputs=moving_image), fixed_image],
                            axis=4)  # [batch, f_dim1, f_dim2, f_dim3, 2]
 
-        h0, hc0 = self._downsample_resnet_block0(inputs=inputs, training=training)
-        h1, hc1 = self._downsample_resnet_block1(inputs=h0, training=training)
-        h2, hc2 = self._downsample_resnet_block2(inputs=h1, training=training)
-        h3, hc3 = self._downsample_resnet_block3(inputs=h2, training=training)
+        h0, hc0 = self._downsample_block0(inputs=images, training=training)
+        h1, hc1 = self._downsample_block1(inputs=h0, training=training)
+        h2, hc2 = self._downsample_block2(inputs=h1, training=training)
+        h3, hc3 = self._downsample_block3(inputs=h2, training=training)
         hm = [self._conv3d_block4(inputs=h3, training=training)]
-        hcs = [hc0, hc1, hc2, hc3]
 
-        for i in range(len(self._upsample_resnet_blocks)):
-            hm.append(self._upsample_resnet_blocks[i](inputs=[hm[i], hcs[3 - i]], training=training))
+        hm += [self._upsample_block3(inputs=[hm[0], hc3], training=training)] if self._ddf_min_level < 4 else []
+        hm += [self._upsample_block2(inputs=[hm[1], hc2], training=training)] if self._ddf_min_level < 3 else []
+        hm += [self._upsample_block1(inputs=[hm[2], hc1], training=training)] if self._ddf_min_level < 2 else []
+        hm += [self._upsample_block0(inputs=[hm[3], hc0], training=training)] if self._ddf_min_level < 1 else []
 
         ddf = tf.reduce_sum(tf.stack([self._ddf_summands[i](inputs=hm[4 - i]) for i in self._ddf_levels], axis=5),
                             axis=5)
+        return ddf
 
-        grid_warped = self._grid_ref + ddf  # [batch, f_dim1, f_dim2, f_dim3, 3]
-        warped_moving_label = util.warp_moving(moving_image_or_label=moving_label,
-                                               grid_warped=grid_warped)  # [batch, f_dim1, f_dim2, f_dim3, 1]
 
-        concatenated = tf.concat([ddf, warped_moving_label], axis=4)
-        return concatenated
+def build_model(moving_image_size, fixed_image_size, batch_size, num_channel_initial):
+    # inputs
+    moving_image = tf.keras.Input(shape=(*moving_image_size,), batch_size=batch_size, name="moving_image")
+    fixed_image = tf.keras.Input(shape=(*fixed_image_size,), batch_size=batch_size, name="fixed_image")
+    moving_label = tf.keras.Input(shape=(*moving_image_size,), batch_size=batch_size, name="moving_label")
+
+    # backbone
+    local_model = LocalModel(moving_image_size=moving_image_size, fixed_image_size=fixed_image_size,
+                             batch_size=batch_size, num_channel_initial=num_channel_initial)
+
+    # ddf
+    ddf = local_model(inputs=[moving_image, fixed_image])
+
+    # prediction
+    warped_moving_label = layer.Warping(fixed_image_size=fixed_image_size)([ddf, moving_label])
+
+    # build model
+    registration_model = tf.keras.Model(inputs=[moving_image, fixed_image, moving_label], outputs=warped_moving_label,
+                                        name="RegModel")
+
+    # add regularization loss
+    bending_loss = tf.reduce_mean(loss.local_displacement_energy(ddf, "bending", 0.5))
+    registration_model.add_loss(bending_loss)
+    return registration_model
