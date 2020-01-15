228147774ac65eed3dc343430cb0cc48ca6f7583
Yunguan Fu <ucabfua@ucl.ac.uk>
2020-01-15 00:36:48 +0000

    remove some prints

--- main.py
+++ main.py
@@ -156,8 +156,8 @@ for epoch in range(num_epochs):
             # update tensorboard
             metrics_train.update_tensorboard(step=optimizer.iterations)
 
-            print('Training loss at step %d: %s' % (step, metrics_train))
-        print('Training loss at epoch %d: %s' % (epoch, metrics_train))
+            # print("Training loss at step %d: %s" % (step, metrics_train))
+        print("Training loss at epoch %d: %s" % (epoch, metrics_train))
 
     # test
     with tb_writer_test.as_default():
@@ -169,7 +169,8 @@ for epoch in range(num_epochs):
             metrics_test.update(metric_value_dict=metric_value_dict_test)
         # update tensorboard
         metrics_test.update_tensorboard(step=optimizer.iterations)
-        print('Test loss at step %d: %s' % (step, metrics_test))
+        print("Test loss at step %d: %s" % (step, metrics_test))
+        print("Test example indices: %s" % indices_test)
 
     # save models
     if epoch % save_period == 0:
