dcb8b510fed77af9723a1bb433bcdf8d7a3428c6
Yunguan Fu <ucabfua@ucl.ac.uk>
2020-01-19 21:52:05 +0000

    backup config

--- src/config/parser.py
+++ src/config/parser.py
@@ -6,3 +6,8 @@ def load(config_path):
         config_path = "src/config/default.yaml"
     with open(config_path) as file:
         return yaml.load(file)
+
+
+def save(config, out_dir):
+    with open(out_dir + "/config.yaml", "w+") as f:
+        f.write(yaml.dump(config))
