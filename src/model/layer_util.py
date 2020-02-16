c0480d9d56caf7af59934d0d111baa4112d303a7
Yunguan Fu <ucabfua@ucl.ac.uk>
2020-02-16 19:51:10 +0000

    warp basic layers

--- src/model/layer_util.py
+++ src/model/layer_util.py
@@ -8,55 +8,6 @@ def he_normal():
     return tf.keras.initializers.he_normal()
 
 
-def act(identifier):
-    """
-    :param identifier: e.g. "relu"
-    :return:
-    """
-    return tf.keras.activations.get(identifier=identifier)
-
-
-def batch_norm(axis=-1):
-    """
-    :param axis: the axis that should be normalized (typically the features axis)
-    :return:
-    """
-    return tf.keras.layers.BatchNormalization(axis=axis)
-
-
-def conv3d(filters, kernel_size=3, strides=1, padding="same", activation=None, use_bias=True,
-           kernel_initializer="glorot_uniform"):
-    """
-    :param filters: number of channels of the output
-    :param kernel_size: e.g. (3,3,3) or 3
-    :param strides: e.g. (1,1,1) or 1
-    :param padding: "valid" or "same"
-    :param activation:
-    :param use_bias:
-    :param kernel_initializer:
-    :return:
-    """
-    return tf.keras.layers.Conv3D(filters=filters,
-                                  kernel_size=kernel_size,
-                                  strides=strides,
-                                  padding=padding,
-                                  activation=activation,
-                                  use_bias=use_bias,
-                                  kernel_initializer=kernel_initializer, )
-
-
-def max_pool3d(pool_size, strides=None, padding="same"):
-    """
-    :param pool_size: e.g. (2,2,2)
-    :param strides: e.g. (2.2.2)
-    :param padding:
-    :return:
-    """
-    return tf.keras.layers.MaxPool3D(pool_size=pool_size,
-                                     strides=strides,
-                                     padding=padding)
-
-
 def check_inputs(inputs, size, msg=""):
     if msg != "":
         msg += " "
