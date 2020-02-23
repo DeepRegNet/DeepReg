5664b5f9e8cf29f40f56092ccd60d0f2a85d36ea
Yunguan Fu <ucabfua@ucl.ac.uk>
2020-02-23 20:49:38 +0000

    use sgd with larger lr

--- src/model/optimizer.py
+++ src/model/optimizer.py
@@ -4,5 +4,7 @@ import tensorflow as tf
 def get_optimizer(tf_opt_config):
     if tf_opt_config["name"] == "adam":
         return tf.keras.optimizers.Adam(**tf_opt_config["adam"])
+    elif tf_opt_config["name"] == "sgd":
+        return tf.keras.optimizers.SGD(**tf_opt_config["sgd"])
     else:
         raise ValueError("Unknown optimizer")
