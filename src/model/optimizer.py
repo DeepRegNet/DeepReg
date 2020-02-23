986943d07e223aa025d8b66b2e6234c850cf7fd7
Yunguan Fu <ucabfua@ucl.ac.uk>
2020-02-23 21:58:48 +0000

    add rms optimizer

--- src/model/optimizer.py
+++ src/model/optimizer.py
@@ -6,5 +6,7 @@ def get_optimizer(tf_opt_config):
         return tf.keras.optimizers.Adam(**tf_opt_config["adam"])
     elif tf_opt_config["name"] == "sgd":
         return tf.keras.optimizers.SGD(**tf_opt_config["sgd"])
+    elif tf_opt_config["name"] == "rms":
+        return tf.keras.optimizers.RMSprop(**tf_opt_config["rms"])
     else:
         raise ValueError("Unknown optimizer")
