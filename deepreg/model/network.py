45b78112e1772f70d7a8a2f7c23a3c0439592afe
Yunguan Fu <ucabfua@ucl.ac.uk>
2020-04-11 19:58:41 +0100

    refactor code

--- deepreg/model/network.py
+++ deepreg/model/network.py
@@ -57,25 +57,37 @@ def build_ddf_model(moving_image_size, fixed_image_size, index_size, batch_size,
     :param tf_loss_config:
     :return:
     """
+
+    def forward(_backbone, _moving_image, _moving_label, _fixed_image):
+        """
+
+        :param _backbone:
+        :param _moving_image: [batch, m_dim1, m_dim2, m_dim3]
+        :param _moving_label: [batch, m_dim1, m_dim2, m_dim3]
+        :return:
+        """
+        # ddf
+        backbone_input = tf.concat([layer.Resize3d(size=fixed_image_size)(inputs=tf.expand_dims(_moving_image, axis=4)),
+                                    tf.expand_dims(_fixed_image, axis=4)],
+                                   axis=4)  # [batch, f_dim1, f_dim2, f_dim3, 2]
+        _ddf = _backbone(inputs=backbone_input)  # [batch, f_dim1, f_dim2, f_dim3, 3]
+
+        # prediction image ang label shape = [batch, f_dim1, f_dim2, f_dim3]
+        _pred_fixed_image = layer.Warping(fixed_image_size=fixed_image_size)([_ddf, _moving_image])
+        _pred_fixed_label = layer.Warping(fixed_image_size=fixed_image_size)([_ddf, _moving_label])
+
+        return _ddf, _pred_fixed_image, _pred_fixed_label
+
     # inputs
     moving_image, fixed_image, moving_label, indices = build_inputs(
         moving_image_size, fixed_image_size, index_size, batch_size)
-
-    backbone_input = tf.concat([layer.Resize3d(size=fixed_image_size)(inputs=tf.expand_dims(moving_image, axis=4)),
-                                tf.expand_dims(fixed_image, axis=4)],
-                               axis=4)  # [batch, f_dim1, f_dim2, f_dim3, 2]
 
     # backbone
     backbone = build_backbone(image_size=fixed_image_size, out_channels=3,
                               tf_model_config=tf_model_config)
 
     # ddf
-    ddf = backbone(inputs=backbone_input)  # [batch, f_dim1, f_dim2, f_dim3, 3]
-
-    # prediction image ang label shape = [batch, f_dim1, f_dim2, f_dim3]
-    pred_fixed_image = layer.Warping(fixed_image_size=fixed_image_size)([ddf, moving_image])
-    pred_fixed_label = layer.Warping(fixed_image_size=fixed_image_size)([ddf, moving_label])
-
+    ddf, pred_fixed_image, pred_fixed_label = forward(backbone, moving_image, moving_label, fixed_image)
     # build model
     model = tf.keras.Model(inputs=[moving_image, fixed_image, moving_label, indices],
                            outputs=[pred_fixed_label],
