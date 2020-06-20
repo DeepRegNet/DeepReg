3ce50acafa13be95b7282da6ad94be2082cf6ed6
Shaheer U Saeed <shaheersd@gmail.com>
2020-06-20 15:05:39 +0100

    Issue #32: add h5 unlabeled loader for paired and unpaired

--- deepreg/data/h5/h5_paired_unlabeled_loader.py
+++ deepreg/data/h5/h5_paired_unlabeled_loader.py
@@ -5,7 +5,7 @@ from deepreg.data.h5.util import H5FileLoader
 from deepreg.data.util import check_difference_between_two_lists
 
 
-class H5PairedLabeledDataLoader(PairedDataLoader, GeneratorDataLoader):
+class H5PairedUnlabeledDataLoader(PairedDataLoader, GeneratorDataLoader):
     
     def __init__(self, data_dir_path: str, sample_label: str, seed, moving_image_shape: (list, tuple), fixed_image_shape):
         """
@@ -19,15 +19,13 @@ class H5PairedLabeledDataLoader(PairedDataLoader, GeneratorDataLoader):
         :param moving_image_shape: (width, height, depth)
         :param fixed_image_shape: (width, height, depth)
         """
-        super(H5PairedLabeledDataLoader, self).__init__(moving_image_shape=moving_image_shape,
+        super(H5PairedUnlabeledDataLoader, self).__init__(moving_image_shape=moving_image_shape,
                                                            fixed_image_shape=fixed_image_shape,
                                                            sample_label=sample_label, seed=seed)
         
         self.loader_moving_image = H5FileLoader(os.path.join(data_dir_path, "moving_images"))
         self.loader_fixed_image = H5FileLoader(os.path.join(data_dir_path, "fixed_images"))
-        self.loader_moving_label = H5FileLoader(os.path.join(data_dir_path, "moving_labels"))
-        self.loader_fixed_label = H5FileLoader(os.path.join(data_dir_path, "fixed_labels"))
-        
+
         self.num_images = len(self.loader_moving_image.get_data_names())
         self.validate_data_files()
         
@@ -38,20 +36,15 @@ class H5PairedLabeledDataLoader(PairedDataLoader, GeneratorDataLoader):
         '''
         data_names_moving_image = self.loader_moving_image.get_data_names()
         data_names_fixed_image = self.loader_fixed_image.get_data_names()
-        data_names_moving_label = self.loader_moving_label.get_data_names()
-        data_names_fixed_label = self.loader_fixed_label.get_data_names()
         
         check_difference_between_two_lists(list1=data_names_moving_image, list2=data_names_fixed_image)
-        check_difference_between_two_lists(list1=data_names_moving_image, list2=data_names_moving_label)
-        check_difference_between_two_lists(list1=data_names_moving_image, list2=data_names_fixed_label)
-        
-    
+   
     def get_generator(self):
         for image_index in range(self.num_images):
             moving_image = self.loader_moving_image.get_data(index=image_index) / 255.
             fixed_image = self.loader_fixed_image.get_data(index=image_index) / 255.
-            moving_label = self.loader_moving_label.get_data(index=image_index)
-            fixed_label = self.loader_fixed_label.get_data(index=image_index) 
+            moving_label = None
+            fixed_label = None
 
             for sample in self.sample_image_label(moving_image=moving_image,
                                                   fixed_image=fixed_image,
@@ -61,3 +54,5 @@ class H5PairedLabeledDataLoader(PairedDataLoader, GeneratorDataLoader):
                 yield sample
 
         
+
+
