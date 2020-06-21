d9a5d0a94a1395671389a4b23fba4f784a3c4177
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-06-21 01:07:26 +0100

    ref #28 affine-transformed fixed image added

--- test/sit_classical_registration.py
+++ test/sit_classical_registration.py
@@ -1,66 +1,64 @@
-
+'''
+Classical iterative pairwise registration algorithms as integration tests
+'''
 import nibabel
-import numpy as np
 import tensorflow as tf 
-import deepreg.model.layer_util as layer_util
 import deepreg.model.layer as layer
 import deepreg.model.loss.image as image_loss
 import deepreg.model.loss.deform as deform_loss
+import deepreg.model.layer_util as layer_util
+import matplotlib.pyplot as plt
 
-load_image = lambda fn:tf.cast(tf.expand_dims(nibabel.load(fn).dataobj, axis=0), dtype=tf.float32)
-moving_image = load_image('./data/mr_us/unpaired/train/images/case000000.nii.gz')
-fixed_image = load_image('./data/mr_us/unpaired/train/images/case000001.nii.gz')
 
+## registration parameters
 image_loss_name = 'ssd'
 deform_loss_name = 'bending'
-weight_deform_loss = 100
+weight_deform_loss = 1e3
+learning_rate = 0.1
+total_iter = int(3000)
 
+
+## load image
+load_image = lambda fn:tf.cast(tf.expand_dims(nibabel.load(fn).dataobj, axis=0), dtype=tf.float32)
+moving_image = load_image('./data/mr_us/unpaired/train/images/case000000.nii.gz')
+# fixed_image = load_image('./data/mr_us/unpaired/train/images/case000001.nii.gz')
+
+# random affine-transformed fixed image
+random_transform = layer_util.random_transform_generator(batch_size=1, scale=0.2)
+grid_ref = layer_util.get_reference_grid(grid_size=moving_image.shape[1:4])
+fixed_image = layer_util.resample(vol=moving_image, loc=layer_util.warp_grid(grid_ref, random_transform))
+
+# ddf as trainable weights
 fixed_image_size = fixed_image.shape
+initialiser = tf.random_normal_initializer(mean=0, stddev=1e-3)
+var_ddf = tf.Variable(initialiser(fixed_image_size+[3]), name='ddf', trainable=True)
 warping = layer.Warping(fixed_image_size=fixed_image_size[1:4])
 
-initialiser = tf.random_normal_initializer(mean=0, stddev=1e-3)
-var_ddf = tf.Variable(
-    initial_value = initialiser(shape=fixed_image_size+[3]),
-    name = 'ddf',
-    trainable=True
-)
 
-
+## optimisation
 @tf.function
-def train_step(wapper, weights, optimizer, mov, fix):
+def train_step(warper, weights, optimizer, mov, fix):
     with tf.GradientTape() as tape:
-        pred = wapper(inputs=[weights, mov])
+        pred = warper(inputs=[weights, mov])
         loss_image = image_loss.similarity_fn(y_true=fix, y_pred=pred, name=image_loss_name)
         loss_deform = deform_loss.local_displacement_energy(weights, deform_loss_name)
         loss = loss_image + weight_deform_loss*loss_deform
-
     gradients = tape.gradient(loss, [weights])
     optimizer.apply_gradients(zip(gradients, [weights]))
     return loss, loss_image, loss_deform
 
-learning_rate = 0.1
-total_iter = int(1000)
 optimizer = tf.optimizers.Adam(learning_rate)
-
-# train
 for step in range(total_iter):
-    loss_train, loss_image_train, loss_deform_train = train_step(warping, var_ddf, optimizer, moving_image, fixed_image)
-
-    if (step % 10) == 0:
-        tf.print('Step', step, 
-        'loss', loss_train, 
-        image_loss_name, loss_image_train, 
-        deform_loss_name, loss_deform_train)
+    loss_opt, loss_image_opt, loss_deform_opt = train_step(warping, var_ddf, optimizer, moving_image, fixed_image)
+    if (step % 50) == 0:  # print info
+        tf.print('Step',step, 'loss',loss_opt, image_loss_name,loss_image_opt, deform_loss_name,loss_deform_opt)
 
 
-# predict
+## predict
 pred_fixed_image = warping(inputs=[var_ddf, moving_image])
 
-import matplotlib.pyplot as plt
-
-print(fixed_image_size)
-
-idx_slices = [20, 25, 30, 35, 40]
+# display
+idx_slices = [int(5+x*5) for x in range(int(fixed_image_size[3]/5)-1)]
 nIdx = len(idx_slices)
 plt.figure()
 for idx in range(len(idx_slices)):
