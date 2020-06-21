27ceceb94a05ec26f0f6ff8609425dfd42365ade
Yunguan Fu <yunguan.fu.18@ucl.ac.uk>
2020-06-21 21:49:08 +0100

    issue 5# sample group with equal chance

--- deepreg/data/nifti/nifti_grouped_loader.py
+++ deepreg/data/nifti/nifti_grouped_loader.py
@@ -9,7 +9,7 @@ from deepreg.data.util import check_difference_between_two_lists
 class NiftiGroupedDataLoader(UnpairedDataLoader, GeneratorDataLoader):
     def __init__(self,
                  data_dir_path: str, labeled: bool, sample_label: str,
-                 intra_group_prob: float, intra_group_option: str,
+                 intra_group_prob: float, intra_group_option: str, sample_image_in_group: bool,
                  seed, image_shape: (list, tuple)):
         """
         Load data which are grouped, labeled or unlabeled
@@ -37,17 +37,25 @@ class NiftiGroupedDataLoader(UnpairedDataLoader, GeneratorDataLoader):
         self.num_groups = len(self.loader_moving_image.group_paths)
         self.num_images_per_group = [len(self.loader_moving_image.file_path_dict[g])
                                      for g in self.loader_moving_image.group_paths]
-        self.intra_sample_indices, self.inter_sample_indices = self.get_sample_indices(intra_group_option)
+        self.intra_group_option = intra_group_option
         self.intra_group_prob = intra_group_prob
-        if intra_group_prob == 0:  # inter only
-            self._num_samples = len(self.inter_sample_indices)
-        elif intra_group_prob == 1:  # intra only
-            self._num_samples = len(self.intra_sample_indices)
-        else:  # mixed
-            if intra_group_prob < 0 or intra_group_prob > 1:
-                raise ValueError("intra_group_prob should be between [0,1]")
-            # TODO this value is too large
-            self._num_samples = max(len(self.inter_sample_indices), len(self.intra_sample_indices))
+        self.sample_image_in_group = sample_image_in_group
+        if self.intra_group_prob < 1:
+            if self.num_groups < 2:
+                raise ValueError("There are <2 groups, can't do inter group sampling")
+        if self.sample_image_in_group:
+            # one image pair in each group (pair) will be yielded
+            self.sample_indices = None
+            self._num_samples = self.num_groups
+        else:
+            # all possible pair in each group (pair) will be yielded
+            if intra_group_prob not in [0, 1]:
+                raise ValueError("Mixing intra and inter groups is not supported when not sampling pairs.")
+            if intra_group_prob == 0:  # inter group
+                sample_indices = self.get_inter_sample_indices()
+            else:  # intra group
+                sample_indices = self.get_intra_sample_indices()
+            self._num_samples = len(sample_indices)
 
     def validate_data_files(self):
         """Verify all loader have the same files"""
@@ -56,47 +64,49 @@ class NiftiGroupedDataLoader(UnpairedDataLoader, GeneratorDataLoader):
             filenames_label = self.loader_moving_label.get_relative_file_paths()
             check_difference_between_two_lists(list1=filenames_image, list2=filenames_label)
 
-    def get_sample_indices(self, intra_group_option):
+    def get_intra_sample_indices(self):
         """
-        Set the sample indices for inter/intra group
+        Set the sample indices for intra group
         One sample index is ((group1, image1), (group2, image2)), where
         - image1 of group1 is moving image
         - image2 of group2 is fixed image
+        assuming group i has ni images
+        then in total there are at most sum(ni ** 2) intra samples
         :return:
         """
-
-        # intra group
-        # assuming group i has ni images
-        # then in total there are at most sum(ni ** 2) intra samples
         intra_sample_indices = []
         for group_index in range(self.num_groups):
             num_images_in_group = self.num_images_per_group[group_index]
-            if intra_group_option == "forward":
+            if self.intra_group_option == "forward":
                 for i in range(num_images_in_group):
                     for j in range(i):
                         # j < i
                         intra_sample_indices.append(((group_index, j), (group_index, i)))
-            elif intra_group_option == "backward":
+            elif self.intra_group_option == "backward":
                 for i in range(num_images_in_group):
                     for j in range(i):
                         # i > j
                         intra_sample_indices.append(((group_index, i), (group_index, j)))
-            elif intra_group_option == "bidirectional":
+            elif self.intra_group_option == "bidirectional":
                 for i in range(num_images_in_group):
                     for j in range(i):
                         # j < i, i > j
                         intra_sample_indices.append(((group_index, j), (group_index, i)))
                         intra_sample_indices.append(((group_index, i), (group_index, j)))
-            elif intra_group_option == "all":
-                for i in range(num_images_in_group):
-                    for j in range(num_images_in_group):
-                        intra_sample_indices.append(((group_index, i), (group_index, j)))
             else:
-                raise ValueError("Unknown intra_group_option, must be forward/backward/bidirectional/all")
+                raise ValueError("Unknown intra_group_option, must be forward/backward/bidirectional")
+        return intra_sample_indices
 
-        # inter group
-        # assuming group i has ni images
-        # then in total there are sum(ni) ** 2 - sum(ni ** 2) inter samples
+    def get_inter_sample_indices(self):
+        """
+        Set the sample indices for inter group
+        One sample index is ((group1, image1), (group2, image2)), where
+        - image1 of group1 is moving image
+        - image2 of group2 is fixed image
+        assuming group i has ni images
+        then in total there are sum(ni) ** 2 - sum(ni ** 2) inter samples
+        :return:
+        """
         inter_sample_indices = []
         for group_index1 in range(self.num_groups):
             for group_index2 in range(self.num_groups):
@@ -105,27 +115,44 @@ class NiftiGroupedDataLoader(UnpairedDataLoader, GeneratorDataLoader):
                 for image_index1 in range(num_images_in_group1):
                     for image_index2 in range(num_images_in_group2):
                         inter_sample_indices.append(((group_index1, image_index1), (group_index2, image_index2)))
-
-        return intra_sample_indices, inter_sample_indices
+        return inter_sample_indices
 
     def sample_index_generator(self):
         rnd = random.Random(self.seed)
-        if self.intra_group_prob == 0 or self.intra_group_prob == 1:  # inter or intra only
-            sample_indices = self.intra_sample_indices.copy() if self.intra_group_prob == 1 \
-                else self.inter_sample_indices.copy()
+        if self.sample_image_in_group:
+            group_indices = [i for i in range(self.num_groups)]
+            if rnd.random() <= self.intra_group_prob:  # intra group, inside one group
+                group_index = rnd.choice(group_indices)
+                num_images_in_group = self.num_images_per_group[group_index]
+                if self.intra_group_option in ["forward", "backward"]:
+                    # image_index1 < image_index2
+                    # image_index1 must be <= num_images_in_group-2
+                    image_index1 = rnd.choice([i for i in range(num_images_in_group - 1)])
+                    image_index2 = rnd.choice([i for i in range(image_index1 + 1, num_images_in_group)])
+                    if self.intra_group_option == "forward":
+                        yield (group_index, image_index1), (group_index, image_index2), [group_index, image_index1,
+                                                                                         group_index, image_index2]
+                    else:
+                        yield (group_index, image_index2), (group_index, image_index1), [group_index, image_index2,
+                                                                                         group_index, image_index1]
+                elif self.intra_group_option == "bidirectional":
+                    image_index1 = rnd.choice([i for i in range(num_images_in_group)])
+                    image_index2 = rnd.choice([i for i in range(num_images_in_group) if i != image_index1])
+                    yield (group_index, image_index1), (group_index, image_index2), [group_index, image_index1,
+                                                                                     group_index, image_index2]
+                else:
+                    raise ValueError("Unknown intra_group_option, must be forward/backward/bidirectional/all")
+            else:  # inter group, between different groups
+                group_index1, group_index2 = rnd.sample(group_indices, 2)
+                num_images_in_group1 = self.num_images_per_group[group_index1]
+                num_images_in_group2 = self.num_images_per_group[group_index2]
+                image_index1 = rnd.choice([i for i in range(num_images_in_group1)])
+                image_index2 = rnd.choice([i for i in range(num_images_in_group2)])
+                yield (group_index1, image_index1), (group_index2, image_index2), [group_index1, image_index1,
+                                                                                   group_index2, image_index2]
+        else:
+            assert self.sample_indices is not None
+            sample_indices = self.sample_indices.copy()
             rnd.shuffle(sample_indices)
             for sample_index in sample_indices:
-                moving_index, fixed_index = sample_index
-                yield moving_index, fixed_index, list(moving_index) + list(fixed_index)
-        else:  # mixed
-            intra_sample_indices = self.intra_sample_indices.copy()
-            inter_sample_indices = self.inter_sample_indices.copy()
-            rnd.shuffle(intra_sample_indices)
-            rnd.shuffle(inter_sample_indices)
-            while len(intra_sample_indices) > 0 and len(inter_sample_indices) > 0:
-                if rnd.random() <= self.intra_group_prob:  # intra group
-                    sample_index = intra_sample_indices.pop(0)
-                else:  # inter group
-                    sample_index = inter_sample_indices.pop(0)
-                moving_index, fixed_index = sample_index
-                yield moving_index, fixed_index, list(moving_index) + list(fixed_index)
+                yield sample_index
