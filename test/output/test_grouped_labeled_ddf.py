47b7ccd4cda361f11ba9a99e33d62a385ba69c10
Nina Montana Brown <nina.brown.15@ucl.ac.uk>
2020-06-24 21:07:25 +0200

    Issue #68: added unit tests in separate files to stage several jobs at the same time

---
+++ test/output/test_grouped_labeled_ddf.py
@@ -0,0 +1,28 @@
+"""
+Unit test for grouped labeled data
+"""
+import os
+import tensorflow as tf
+from deepreg.predict import predict
+from deepreg.train import train
+
+def test_grouped_labeled_ddf():
+    tf.get_logger().setLevel(3)
+
+    def train_and_predict_with_config(test_name, config_path):
+        """
+        Function that helps unit test whether a session with a given config
+        runs the train and predict functions
+        """
+        tf.keras.backend.clear_session()  # needed for pytest, otherwise model output name will change
+        gpu = ""
+        gpu_allow_growth = False
+        ckpt_path = ""
+        log_dir = os.path.join("logs", "test_" + test_name)
+        train(gpu=gpu, config_path=config_path, gpu_allow_growth=gpu_allow_growth, ckpt_path=ckpt_path, log_dir=log_dir)
+        ckpt_path = os.path.join(log_dir, "save", "weights-epoch2.ckpt")
+        predict(gpu=gpu, gpu_allow_growth=gpu_allow_growth, ckpt_path=ckpt_path, mode="test",
+                batch_size=1, log_dir=log_dir, sample_label="all")
+
+    train_and_predict_with_config(test_name="grouped_labeled_ddf",
+                                    config_path="/deepreg/config/grouped_labeled_ddf.yaml")
