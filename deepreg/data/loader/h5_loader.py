da6961f1e73a03e48a5c6871d50c60fbe4b79ae6
Shaheer U Saeed <shaheersd@gmail.com>
2020-06-25 08:18:19 +0100

    Issue #32: fix directory structure and correponding h5_loader functions

--- deepreg/data/loader/h5_loader.py
+++ deepreg/data/loader/h5_loader.py
@@ -16,19 +16,18 @@ class H5FileLoader(FileLoader):
     def __init__(self, dir_path: str, grouped: bool):
         super(H5FileLoader, self).__init__(dir_path=dir_path, grouped=grouped)
         self.file_paths = get_sorted_filenames_in_dir(dir_path=dir_path, suffix="h5")
+        
+        self.dir_path = '/'.join(dir_path.split('/')[:-1])
+        self.fname = dir_path.split('/')[-1] + '.h5'
+        
 
         self.data_dict = None
 
         if grouped:
-            self.group_names = sorted(os.listdir(dir_path))
-            self.num_groups_in_dir = len(os.listdir(self.dir_path))
+            self.num_groups = self.get_num_groups()
 
-            self.group_paths = sorted(os.listdir(dir_path))
-            for i in range(len(self.group_paths)):
-                self.group_paths[i] = os.path.join(self.dir_path,
-                                                   self.group_paths[i])
 
-    def dict_from_h5(self, fname='data.h5'):
+    def dict_from_h5(self, fname):
         """
         Dictionary is generated from data in an h5 file
         
@@ -41,14 +40,11 @@ class H5FileLoader(FileLoader):
                 data_dict[key] = np.asarray(f[key], dtype=np.float32)
         self.data_dict = data_dict
 
-    def get_data_names(self, fname=None):
+    def get_data_names(self):
         """
         Return list of the names of data in the h5 file
         """
-        if fname is None:
-            self.dict_from_h5(fname='data.h5')
-        else:
-            self.dict_from_h5(fname=fname)
+        self.dict_from_h5(fname=self.fname)
         return list(self.data_dict.keys())
 
     def get_data(self, index: (int, tuple)):
@@ -59,21 +55,16 @@ class H5FileLoader(FileLoader):
         
         :returns arr: the data array at the specified index
         """
-        dir_path_orig = self.dir_path
         if isinstance(index, int):
             assert not self.grouped
-            self.dict_from_h5()
+            self.dict_from_h5(fname=self.fname)
             assert 0 <= index < len(self.data_dict)
             data_name = list(self.data_dict.keys())[index]
         elif isinstance(index, tuple):
             assert self.grouped
             group_index, sample_index = index
-            group_name = self.group_names[group_index]
-            self.dir_path = os.path.join(dir_path_orig, group_name)
-            self.dict_from_h5(fname=group_name + '.h5')
-            data_name = list(self.data_dict.keys())[sample_index]
-            self.dir_path = dir_path_orig
-
+            data_name = 'group-' + str(group_index+1) + '-' + str(sample_index+1)
+            self.dict_from_h5(fname=self.fname)
         arr = self.data_dict[data_name]
         if len(arr.shape) == 4 and arr.shape[3] == 1:
             # for labels, if there's only one label, remove the last dimension
@@ -85,16 +76,9 @@ class H5FileLoader(FileLoader):
         for grouped folder structure return the names of samples
         :returns: sorted list of all sample names
         """
-        dir_path_orig = self.dir_path
-        all_sample_names = []
-        for group_name in self.group_names:
-            self.dir_path = os.path.join(dir_path_orig, group_name)
-            self.dict_from_h5(fname=group_name + '.h5')
-            samples_in_group = sorted(self.get_data_names(fname=
-                                                          group_name + '.h5'))
-            all_sample_names = all_sample_names + samples_in_group
-            self.dir_path = dir_path_orig
-        return sorted(all_sample_names)
+        self.dict_from_h5(fname=self.fname)
+        all_sample_names = sorted(self.data_dict.keys())
+        return all_sample_names
 
     def get_data_ids(self):
         if self.grouped:
@@ -107,18 +91,27 @@ class H5FileLoader(FileLoader):
 
     def get_num_images_per_group(self):
         assert self.grouped
-        dir_path_orig = self.dir_path
+        self.dict_from_h5(fname=self.fname)
+        all_images = sorted(self.data_dict.keys())
+        group_numbers = []
+        images_numbers = []
+        for image in all_images:
+            image_number = int(image.split('-')[-1])
+            group_number = int(image.split('-')[-2])
+            group_numbers.append(group_number)
         num_images_per_group = []
-        for group_name in self.group_names:
-            self.dir_path = os.path.join(dir_path_orig, group_name)
-            self.dict_from_h5(fname=group_name + '.h5')
-            samples_in_group = sorted(self.get_data_names(fname=
-                                                          group_name + '.h5'))
-            num_samples = len(samples_in_group)
-            num_images_per_group.append(num_samples)
-            self.dir_path = dir_path_orig
+        for i in range(self.num_groups):
+            num_images = int(len(np.where(np.array(group_numbers)==i+1)[0] + 1))
+            num_images_per_group.append(num_images)
         return num_images_per_group
 
     def get_num_groups(self) -> int:
         assert self.grouped
-        return self.num_groups_in_dir
+        self.dict_from_h5(fname=self.fname)
+        all_images = sorted(self.data_dict.keys())
+        group_numbers = []
+        for image in all_images:
+            group_number = int(image.split('-')[-2])
+            group_numbers.append(group_number)
+        num_groups = np.amax(np.array(group_numbers))
+        return num_groups
