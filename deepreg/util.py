053024dd0935c9f827bb0dd0c1e51fdfd55dfe47
Yunguan Fu <yunguan.fu.18@ucl.ac.uk>
2020-06-27 18:38:18 +0100

    fix log_dir in tests

--- deepreg/util.py
+++ deepreg/util.py
@@ -20,7 +20,7 @@ def train_and_predict_with_config(test_name, config_path):
     gpu = ""
     gpu_allow_growth = False
     ckpt_path = ""
-    log_dir = "test_" + test_name
+    log_dir = "pytest_train_" + test_name
     train(
         gpu=gpu,
         config_path=config_path,
@@ -28,7 +28,8 @@ def train_and_predict_with_config(test_name, config_path):
         ckpt_path=ckpt_path,
         log_dir=log_dir,
     )
-    ckpt_path = os.path.join(log_dir, "save", "weights-epoch2.ckpt")
+    ckpt_path = os.path.join("logs", log_dir, "save", "weights-epoch2.ckpt")
+    log_dir = "pytest_predict_" + test_name
     predict(
         gpu=gpu,
         gpu_allow_growth=gpu_allow_growth,
