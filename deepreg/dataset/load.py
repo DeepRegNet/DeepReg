0ca72d78cac7e089d8e49fb3e02023f5959c1079
Yunguan Fu <yunguan.fu.18@ucl.ac.uk>
2020-06-28 16:50:50 +0100

    issue #77 add concatenated data loader

--- deepreg/dataset/load.py
+++ deepreg/dataset/load.py
@@ -2,6 +2,7 @@ import os
 
 from deepreg.dataset.loader.grouped_loader import GroupedDataLoader
 from deepreg.dataset.loader.h5_loader import H5FileLoader
+from deepreg.dataset.loader.interface import ConcatenatedDataLoader
 from deepreg.dataset.loader.nifti_loader import NiftiFileLoader
 from deepreg.dataset.loader.paired_loader import PairedDataLoader
 from deepreg.dataset.loader.unpaired_loader import UnpairedDataLoader
@@ -53,11 +54,26 @@ def get_data_loader(data_config, mode):
         file_loader=file_loader, labeled=labeled, sample_label=sample_label, seed=seed
     )
 
+    data_dir_paths = [os.path.join(data_dir, mode)]
+
+    data_loaders = []
+    for data_dir_path in data_dir_paths:
+        data_loader_i = get_single_data_loader(
+            data_type, data_config, common_args, data_dir_path
+        )
+        data_loaders.append(data_loader_i)
+    if len(data_loaders) == 1:
+        return data_loaders[0]
+    else:
+        return ConcatenatedDataLoader(data_loaders=data_loaders)
+
+
+def get_single_data_loader(data_type, data_config, common_args, data_dir_path):
     if data_type == "paired":
         moving_image_shape = data_config["moving_image_shape"]
         fixed_image_shape = data_config["fixed_image_shape"]
         return PairedDataLoader(
-            data_dir_path=os.path.join(data_dir, mode),
+            data_dir_path=data_dir_path,
             moving_image_shape=moving_image_shape,
             fixed_image_shape=fixed_image_shape,
             **common_args,
@@ -68,7 +84,7 @@ def get_data_loader(data_config, mode):
         intra_group_option = data_config["intra_group_option"]
         sample_image_in_group = data_config["sample_image_in_group"]
         return GroupedDataLoader(
-            data_dir_path=os.path.join(data_dir, mode),
+            data_dir_path=data_dir_path,
             intra_group_prob=intra_group_prob,
             intra_group_option=intra_group_option,
             sample_image_in_group=sample_image_in_group,
@@ -78,9 +94,7 @@ def get_data_loader(data_config, mode):
     elif data_type == "unpaired":
         image_shape = data_config["image_shape"]
         return UnpairedDataLoader(
-            data_dir_path=os.path.join(data_dir, mode),
-            image_shape=image_shape,
-            **common_args,
+            data_dir_path=data_dir_path, image_shape=image_shape, **common_args
         )
     else:
         raise ValueError(
