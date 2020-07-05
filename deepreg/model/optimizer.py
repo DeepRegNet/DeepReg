614be915764fa88aea41e162003b5069306a1c09
Nina Montana Brown <nina.brown.15@ucl.ac.uk>
2020-07-05 21:52:06 +0200

    Issue #93: added more info to function docstring and added assertion test to check config is a dict

--- deepreg/model/optimizer.py
+++ deepreg/model/optimizer.py
@@ -9,12 +9,17 @@ import tensorflow as tf
 def get_optimizer(optimizer_config):
     """
     Parsing the optimiser options and parameters
+    from config dictionary.
 
-    :param optimizer_config: unpacked dictionary for the optimiser returned from yaml.load, optimiser options and parameters
+    :param optimizer_config: unpacked dictionary for
+    the optimiser returned from yaml.load, optimiser options
+    and parameters
 
     :return: tf.keras.optimizers object
 
     """
+    assert isinstance(optimizer_config, dict)
+
     if optimizer_config["name"] == "adam":
         return tf.keras.optimizers.Adam(**optimizer_config["adam"])
     elif optimizer_config["name"] == "sgd":
