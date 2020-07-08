fa6d5c60db05682b8d1efc7a90146db72624307c
Nina Montana Brown <nina.brown.15@ucl.ac.uk>
2020-07-08 20:28:58 +0200

    Issue #94: [ci skip] linted and expanded on docs for backbone classes

--- deepreg/model/backbone/u_net.py
+++ deepreg/model/backbone/u_net.py
@@ -11,10 +11,18 @@ https://arxiv.org/pdf/1505.04597.pdf
 
 import tensorflow as tf
 
-from deepreg.model import layer as layer
+from deepreg.model import layer
 
 
 class UNet(tf.keras.Model):
+    """
+    Class that implements UNet based on:
+    O. Ronneberger, P. Fischer, and T. Brox,
+    “U-net: Convolutional networks for biomedical image segmentation,”,
+    Lecture Notes in Computer Science, 2015, vol. 9351, pp. 234–241.
+    https://arxiv.org/pdf/1505.04597.pdft
+    """
+
     def __init__(
         self,
         image_size,
@@ -36,25 +44,27 @@ class UNet(tf.keras.Model):
         :param depth: int, input is at level 0, bottom is at level depth
         :param out_kernel_initializer: str, which kernel to use as initialiser
         :param out_activation: str, activation at last layer
-        :param pooling: Boolean, for downsampling, use non-parameterized pooling if true, otherwise use conv3d
-        :param concat_skip: Boolean, when upsampling, concatenate skipped tensor if true, otherwise use addition
+        :param pooling: Boolean, for downsampling, use non-parameterized
+                        pooling if true, otherwise use conv3d
+        :param concat_skip: Boolean, when upsampling, concatenate skipped
+                            tensor if true, otherwise use addition
         :param kwargs:
         """
         super(UNet, self).__init__(**kwargs)
 
         # init layer variables
-        nc = [num_channel_initial * (2 ** d) for d in range(depth + 1)]
+        num_channels = [num_channel_initial * (2 ** d) for d in range(depth + 1)]
 
         self._num_channel_initial = num_channel_initial
         self._depth = depth
         self._downsample_blocks = [
-            layer.DownSampleResnetBlock(filters=nc[d], pooling=pooling)
+            layer.DownSampleResnetBlock(filters=num_channels[d], pooling=pooling)
             for d in range(depth)
         ]
-        self._bottom_conv3d = layer.Conv3dBlock(filters=nc[depth])
-        self._bottom_res3d = layer.Residual3dBlock(filters=nc[depth])
+        self._bottom_conv3d = layer.Conv3dBlock(filters=num_channels[depth])
+        self._bottom_res3d = layer.Residual3dBlock(filters=num_channels[depth])
         self._upsample_blocks = [
-            layer.UpSampleResnetBlock(filters=nc[d], concat=concat_skip)
+            layer.UpSampleResnetBlock(filters=num_channels[d], concat=concat_skip)
             for d in range(depth)
         ]
         self._output_conv3d = layer.Conv3dWithResize(
@@ -77,8 +87,8 @@ class UNet(tf.keras.Model):
 
         # down sample
         skips = []
-        for d in range(self._depth):  # level 0 to D-1
-            down_sampled, skip = self._downsample_blocks[d](
+        for d_var in range(self._depth):  # level 0 to D-1
+            down_sampled, skip = self._downsample_blocks[d_var](
                 inputs=down_sampled, training=training
             )
             skips.append(skip)
@@ -90,9 +100,9 @@ class UNet(tf.keras.Model):
         )
 
         # up sample, level D-1 to 0
-        for d in range(self._depth - 1, -1, -1):
-            up_sampled = self._upsample_blocks[d](
-                inputs=[up_sampled, skips[d]], training=training
+        for d_var in range(self._depth - 1, -1, -1):
+            up_sampled = self._upsample_blocks[d_var](
+                inputs=[up_sampled, skips[d_var]], training=training
             )
 
         # output
