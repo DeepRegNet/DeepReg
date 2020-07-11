b4450890b23c2c660a9813e9d41b6883e448d969
Adria <a.casamitjana@ucl.ac.uk>
2020-07-11 21:11:07 +0100

    Initial commit

---
+++ demos/predict.py
@@ -0,0 +1,304 @@
+"""
+Module to perform predictions on data using
+command line interface
+"""
+
+import logging
+import os
+from datetime import datetime
+
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import nibabel as nib
+
+import deepreg.config.parser as config_parser
+import deepreg.data.load as load
+import deepreg.model.layer_util as layer_util
+import deepreg.model.loss.label as label_loss
+import deepreg.model.optimizer as opt
+from deepreg.model.network.build import build_model
+
+EPS = 1.0e-6
+
+def init(log_dir):
+    """
+    Function to create new directory to log directory
+    to store results.
+    :param log_dir: string, path to store logs.
+    """
+    # init log directory
+    if log_dir == "":  # default
+        log_dir = os.path.join("logs", datetime.now().strftime("%Y%m%d-%H%M%S"))
+    if os.path.exists(log_dir):
+        logging.warning("Log directory {} exists already.".format(log_dir))
+    else:
+        os.makedirs(log_dir)
+
+
+def predict_on_dataset(dataset, fixed_grid_ref, model, save_dir):
+    """
+    Function to predict results from a dataset from some model
+    :param dataset: where data is stored
+    :param fixed_grid_ref:
+    :param model:
+    :param save_dir: str, path to store dir
+    """
+    metric_map = dict()  # map[image_index][label_index][metric_name] = metric_value
+    for _, inputs_dict in enumerate(dataset):
+
+        outputs_dict = model.predict(x=inputs_dict)
+
+        moving_image = inputs_dict.get("moving_image")
+        fixed_image = inputs_dict.get("fixed_image")
+        indices = inputs_dict.get("indices")
+        moving_label = inputs_dict.get("moving_label", None)
+        fixed_label = inputs_dict.get("fixed_label", None)
+
+
+        ddf = outputs_dict.get("ddf", None)
+        pred_fixed_image = outputs_dict.get("pred_fixed_image", None)
+        pred_fixed_label = outputs_dict.get("pred_fixed_label", None)
+
+        labeled = moving_label is not None
+
+        num_samples = moving_image.shape[0]
+        moving_depth = moving_image.shape[-1]
+        fixed_depth = fixed_image.shape[-1]
+
+        for sample_index in range(num_samples):
+            indices_i = indices[sample_index, :].numpy().astype(int).tolist()
+            image_index = "_".join([str(x) for x in indices_i[:-1]])
+            label_index = str(indices_i[-1])
+
+            # save fixed
+            image_dir = os.path.join(save_dir, "image%s" % image_index)
+            if labeled:
+                image_dir = os.path.join(save_dir, "label%s" % label_index)
+
+            # save volumes
+            print(type(fixed_image))
+            affine = np.eye(4)
+            fixed_image_d = fixed_image[sample_index]
+            moving_image_d = moving_image[sample_index]
+            pred_fixed_image_d = pred_fixed_image[sample_index]
+            img = nib.Nifti1Image(fixed_image_d.numpy(), affine)
+            nib.save(img, os.path.join(image_dir, 'fixed_image.nii.gz'))
+            img = nib.Nifti1Image(moving_image_d.numpy(), affine)
+            nib.save(img, os.path.join(image_dir, 'moving_image.nii.gz'))
+            img = nib.Nifti1Image(pred_fixed_image_d, affine)
+            nib.save(img, os.path.join(image_dir, 'pred_fixe_image.nii.gz'))
+
+            filename_format = os.path.join(image_dir, "depth{depth_index:d}_{name:s}.png")
+            if not os.path.exists(image_dir):
+                os.makedirs(image_dir)
+            for fixed_depth_index in range(fixed_depth):
+                fixed_image_d = fixed_image[sample_index, fixed_depth_index]
+                plt.imsave(
+                    filename_format.format(depth_index=fixed_depth_index, name="fixed_image"),
+                    fixed_image_d, cmap='gray')  # value range for h5 and nifti might be different
+
+                if labeled:
+                    fixed_label_d = fixed_label[sample_index, fixed_depth_index]
+                    fixed_pred_d = pred_fixed_label[sample_index, fixed_depth_index]
+
+                    plt.imsave(
+                        filename_format.format(depth_index=fixed_depth_index, name="fixed_label"),
+                        fixed_label_d, vmin=0, vmax=1, cmap='gray')
+                    plt.imsave(
+                        filename_format.format(depth_index=fixed_depth_index,
+                                               name="fixed_label_pred"),
+                        fixed_pred_d, vmin=0, vmax=1, cmap='gray')
+
+            # save moving
+            if not os.path.exists(image_dir):
+                os.makedirs(image_dir)
+            for moving_depth_index in range(moving_depth):
+                moving_image_d = moving_image[sample_index,...,moving_depth_index]
+                plt.imsave(
+                    filename_format.format(depth_index=moving_depth_index, name="moving_image"),
+                    moving_image_d, cmap='gray')  # value range for h5 and nifti might be different
+                if labeled:
+                    moving_label_d = moving_label[sample_index, ...,moving_depth_index]
+                    plt.imsave(
+                        filename_format.format(depth_index=moving_depth_index, name="moving_label"),
+                        moving_label_d, vmin=0, vmax=1, cmap='gray')
+
+            # save prediction
+            if not os.path.exists(image_dir):
+                os.makedirs(image_dir)
+            for moving_depth_index in range(moving_depth):
+                moving_image_d = pred_fixed_image[sample_index, ..., moving_depth_index]
+                plt.imsave(
+                    filename_format.format(depth_index=moving_depth_index, name="pred_fixed_image"),
+                    moving_image_d, cmap='gray')  # value range for h5 and nifti might be different
+                if labeled:
+                    moving_label_d = pred_fixed_label[sample_index, ..., moving_depth_index]
+                    plt.imsave(
+                        filename_format.format(depth_index=moving_depth_index, name="pred_fixed_label"),
+                        moving_label_d, vmin=0, vmax=1, cmap='gray')
+
+            # save ddf if exists
+            if ddf is not None:
+                if not os.path.exists(image_dir):
+                    os.makedirs(image_dir)
+                for fixed_depth_index in range(fixed_depth):
+                    ddf_d = ddf[sample_index, :, :, fixed_depth_index, :]  # [f_dim1, f_dim2,  3]
+                    print(np.max(ddf_d))
+                    ddf_max, ddf_min = np.max(ddf_d), np.min(ddf_d)
+                    ddf_d = (ddf_d - ddf_min) / np.maximum(ddf_max - ddf_min, EPS)
+                    plt.imsave(
+                        filename_format.format(depth_index=fixed_depth_index, name="ddf"),
+                        ddf_d)
+
+            # calculate metric
+            if labeled:
+                label = fixed_label[sample_index:(sample_index + 1), :, :, :]
+                pred = pred_fixed_label[sample_index:(sample_index + 1), :, :, :]
+                dice = label_loss.dice_score(y_true=label, y_pred=pred, binary=True)
+                dist = label_loss.compute_centroid_distance(y_true=label, y_pred=pred,
+                                                            grid=fixed_grid_ref)
+
+                # save metric
+                if image_index not in metric_map.keys():
+                    metric_map[image_index] = dict()
+                # label should not be repeated - assert that it is not in keys
+                assert label_index not in metric_map[image_index].keys()
+                metric_map[image_index][label_index] = dict(dice=dice.numpy()[0],
+                                                            dist=dist.numpy()[0])
+
+    # print metric
+    line_format = "{image_index:s}, label {label_index:s}, dice {dice:.4f}, dist {dist:.4f}\n"
+    with open(save_dir + "/metric.log", "w+") as file:
+        for image_index in sorted(metric_map.keys()):
+            for label_index in sorted(metric_map[image_index].keys()):
+                file.write(line_format.format(image_index=image_index,
+                                              label_index=label_index,
+                                              **metric_map[image_index][label_index]))
+
+
+def predict(gpu, gpu_allow_growth, ckpt_path, mode, batch_size, log_dir, sample_label):
+    """
+    Function to predict some metrics from the saved model and logging results.
+    :param gpu: str, which env gpu to use.
+    :param gpu_allow_growth: bool, whether to allow gpu growth or not
+    :param ckpt_path: str, where model is stored, should be like
+                      log_folder/save/xxx.ckpt
+    :param mode: which mode to load the data ??
+    :param batch_size: int, batch size to perform predictions in
+    :param log_dir: str, path to store logs
+    :param sample_label:
+    """
+    logging.error("TODO sample_label is not used in predict")
+    # sanity check
+    if not ckpt_path.endswith(".ckpt"):
+        raise ValueError("checkpoint path should end with .ckpt")
+
+    # env vars
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpu if gpu != '-1' else ''
+    os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "false" if gpu_allow_growth else "true"
+
+    # load config
+    config = config_parser.load("/".join(ckpt_path.split("/")[:-2]) + "/config.yaml")
+    data_config = config["data"]
+    tf_data_config = config["tf"]["data"]
+    tf_data_config["batch_size"] = batch_size
+    tf_opt_config = config["tf"]["opt"]
+    tf_model_config = config["tf"]["model"]
+    tf_loss_config = config["tf"]["loss"]
+
+    # data
+    data_loader = load.get_data_loader(data_config, mode)
+    dataset = data_loader.get_dataset_and_preprocess(training=False, repeat=False, **tf_data_config)
+
+    # optimizer
+    optimizer = opt.get_optimizer(tf_opt_config)
+
+    # model
+    model = build_model(moving_image_size=data_loader.moving_image_shape,
+                        fixed_image_size=data_loader.fixed_image_shape,
+                        index_size=data_loader.num_indices,
+                        labeled=data_config["labeled"],
+                        batch_size=tf_data_config["batch_size"],
+                        tf_model_config=tf_model_config,
+                        tf_loss_config=tf_loss_config)
+
+    # metrics
+    model.compile(optimizer=optimizer)
+
+    # load weights
+    # https://stackoverflow.com/questions/58289342/tf2-0-translation-model-error-when-restoring-the-saved-model-unresolved-objec
+    model.load_weights(ckpt_path).expect_partial()
+
+    # predict
+    fixed_grid_ref = layer_util.get_reference_grid(grid_size=data_loader.fixed_image_shape)
+    predict_on_dataset(dataset=dataset, fixed_grid_ref=fixed_grid_ref, model=model,
+                       save_dir=log_dir + "/test")
+
+
+def main(args=None):
+    """
+    Function to run in command line with argparse to predict results on data
+    for a given model
+    """
+    parser = argparse.ArgumentParser(description="predict",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    ## ADD POSITIONAL ARGUMENTS
+    parser.add_argument("--gpu",
+                        "-g",
+                        help="GPU index for training, multiple gpus can be passed",
+                        type=str,
+                        # nargs='+',
+                        default="-1")
+
+    parser.add_argument("--gpu_allow_growth",
+                        "-gr",
+                        help="Prevent TensorFlow from reserving all available GPU memory",
+                        default=False)
+
+    parser.add_argument("--ckpt_path",
+                        "-k",
+                        help="Path of checkpointed model to load",
+                        default="",
+                        type=str,
+                        required=True)
+
+    parser.add_argument('--mode',
+                        "-m",
+                        help="Define the split of data to be used for prediction. One of train / valid / test",
+                        default="test",
+                        required=True)
+
+    parser.add_argument("--batch_size",
+                        "-b",
+                        help="Batch size for predictions",
+                        default=1,
+                        type=int)
+
+    parser.add_argument("--log_dir",
+                        "-l",
+                        help="Path of log directory",
+                        default="",
+                        type=str,
+                        required=True)
+
+    parser.add_argument("--sample_label",
+                        "-s",
+                        help="Method of sampling labels",
+                        default="all",
+                        type=str)
+
+    args = parser.parse_args(args)
+
+    predict(args.gpu,
+            args.gpu_allow_growth,
+            args.ckpt_path,
+            args.mode,
+            args.batch_size,
+            args.log_dir,
+            args.sample_label)
+
+
+if __name__ == "__main__":
+    main()
---
+++ demos/toy_example.py
@@ -0,0 +1,181 @@
+"""
+Module to train a network using init files and a CLI
+"""
+
+import logging
+import os
+from datetime import datetime
+
+import argparse
+import tensorflow as tf
+
+import deepreg.config.parser as config_parser
+import deepreg.model.optimizer as opt
+from deepreg.data.load import get_data_loader
+from deepreg.model.network.build import build_model
+
+
+def init(config_path, log_dir, ckpt_path):
+    """
+    Function to initialise log directories,
+    assert that checkpointed model is the right
+    type and to parse the configuration for training
+    :param config_path: str, path to config file
+    :param log_dir: str, path to where training logs
+                    to be stored.
+    :param ckpt_path: str, path where model is stored.
+    """
+    # init log directory
+    if log_dir == "":  # default
+        log_dir = os.path.join("logs", datetime.now().strftime("%Y%m%d-%H%M%S"))
+    if os.path.exists(log_dir):
+        logging.warning("Log directory {} exists already.".format(log_dir))
+    else:
+        os.makedirs(log_dir)
+
+    # check checkpoint path
+    if ckpt_path != "":
+        if not ckpt_path.endswith(".ckpt"):
+            raise ValueError("checkpoint path should end with .ckpt")
+
+    # load and backup config
+    config = config_parser.load(config_path)
+
+    config_parser.save(config=config, out_dir=log_dir)
+    return config, log_dir
+
+
+def train(gpu, config_path, gpu_allow_growth, ckpt_path, log_dir):
+    """
+    Function to train a model
+    :param gpu: str, which local gpu to use to train
+    :param config_path: str, path to configuration set up
+    :param gpu_allow_growth: bool, whether or not to allocate
+                             whole GPU memory to training
+    :param ckpt_path: str, where to store training ckpts
+    :param log_dir: str, where to store logs in training
+    """
+
+    # env vars
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpu if gpu != '-1' else ''
+    os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true" if gpu_allow_growth else "false"
+
+    # load config
+    config, log_dir = init(config_path, log_dir, ckpt_path)
+    data_config = config["data"]
+    tf_data_config = config["tf"]["data"]
+    tf_opt_config = config["tf"]["opt"]
+    tf_model_config = config["tf"]["model"]
+    tf_loss_config = config["tf"]["loss"]
+    num_epochs = config["tf"]["epochs"]
+    save_period = config["tf"]["save_period"]
+    histogram_freq = config["tf"]["histogram_freq"]
+
+    # data
+    data_loader_train = get_data_loader(data_config, "train")
+    data_loader_val = get_data_loader(data_config, "valid")
+    dataset_train = data_loader_train.get_dataset_and_preprocess(training=True,
+                                                                 repeat=True,
+                                                                 **tf_data_config)
+    dataset_val = data_loader_val.get_dataset_and_preprocess(training=False,
+                                                             repeat=True,
+                                                             **tf_data_config)
+    dataset_size_train = data_loader_train.num_samples
+    dataset_size_val = data_loader_val.num_samples
+    steps_per_epoch_train = max(dataset_size_train // tf_data_config["batch_size"], 1)
+    steps_per_epoch_valid = max(dataset_size_val // tf_data_config["batch_size"], 1)
+
+
+    strategy = tf.distribute.MirroredStrategy()
+    with strategy.scope():
+        # model
+        model = build_model(moving_image_size=data_loader_train.moving_image_shape,
+                            fixed_image_size=data_loader_train.fixed_image_shape,
+                            index_size=data_loader_train.num_indices,
+                            labeled=data_config["labeled"],
+                            batch_size=tf_data_config["batch_size"],
+                            tf_model_config=tf_model_config,
+                            tf_loss_config=tf_loss_config)
+        model.summary()
+
+        # compile
+        optimizer = opt.get_optimizer(tf_opt_config)
+
+        model.compile(optimizer=optimizer)
+        print(model.summary())
+
+        # load weights
+        if ckpt_path is None:
+            model.load_weights(ckpt_path)
+
+        # train
+        # callbacks
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir,
+                                                              histogram_freq=histogram_freq)
+        checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=log_dir + "/save/weights-epoch{epoch:d}.ckpt", save_weights_only=True,
+            period=save_period)
+        # it's necessary to define the steps_per_epoch and validation_steps to prevent errors like
+        # BaseCollectiveExecutor::StartAbort Out of range: End of sequence
+        model.fit(
+            x=dataset_train,
+            steps_per_epoch=steps_per_epoch_train,
+            epochs=num_epochs,
+            validation_data=dataset_val,
+            validation_steps=steps_per_epoch_valid,
+            callbacks=[tensorboard_callback, checkpoint_callback],
+        )
+
+
+def main(args=None):
+    """Entry point for train script"""
+
+    parser = argparse.ArgumentParser(description="train",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    ## ADD POSITIONAL ARGUMENTS
+    parser.add_argument("--config_path",
+                        "-c",
+                        help="Path of config",
+                        type=str,
+                        required=True)
+
+
+    parser.add_argument("--gpu",
+                        "-g",
+                        help="GPU index for training, multiple gpus can be passed",
+                        default='-1',
+                        type=str,
+                        nargs='+',
+                        required=False)
+
+    parser.add_argument("--gpu_allow_growth",
+                        "-gr",
+                        help="Prevent TensorFlow from reserving all available GPU memory",
+                        default=False)
+
+    parser.add_argument("--ckpt_path",
+                        "-k",
+                        help="Path of checkpointed model to load",
+                        default="",
+                        type=str,
+                        required=False)
+
+    parser.add_argument("--log_dir",
+                        "-l",
+                        help="Path of log directory",
+                        default="",
+                        type=str,
+                        required=False)
+
+
+    args = parser.parse_args(args)
+    train(args.gpu,
+          args.config_path,
+          args.gpu_allow_growth,
+          args.ckpt_path,
+          args.log_dir)
+
+
+if __name__ == "__main__":
+    main()
