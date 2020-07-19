89b7f9d03ec01708c9f0f83b2fd7ee1f675383d4
Yunguan Fu <yunguan.fu.18@ucl.ac.uk>
2020-07-19 18:44:54 +0100

    Issue #170: fix get_optimizer rename

--- test/unit/test_model_optimizer.py
+++ test/unit/test_model_optimizer.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 import deepreg.model.optimizer as optimizer
 
 
-def test_get_optimizer_not_dict():
+def test_build_optimizer_not_dict():
     """
     Test assertion error raised if
     config passed not dict.
@@ -19,43 +19,43 @@ def test_get_optimizer_not_dict():
         optimizer.build_optimizer(["name"])
 
 
-def test_get_optimizer_adam():
+def test_build_optimizer_adam():
     """Assert that correct keras optimizer
     is returned when passing the adam string
-    into get_optimizer function
+    into build_optimizer function
     """
     dict_config = {"name": "adam", "adam": {}}
-    opt_get = optimizer.get_optimizer(dict_config)
+    opt_get = optimizer.build_optimizer(dict_config)
     assert isinstance(opt_get, tf.keras.optimizers.Adam)
 
 
-def test_get_optimizer_sgd():
+def test_build_optimizer_sgd():
     """
     Assert that correct keras optimizer
     is returned when passing the sgd string
-    into get_optimizer function
+    into build_optimizer function
     """
     dict_config = {"name": "sgd", "sgd": {}}
-    opt_get = optimizer.get_optimizer(dict_config)
+    opt_get = optimizer.build_optimizer(dict_config)
     assert isinstance(opt_get, tf.keras.optimizers.SGD)
 
 
-def test_get_optimizer_rms():
+def test_build_optimizer_rms():
     """
     Assert that correct keras optimizer
     is returned when passing the rms string
-    into get_optimizer function
+    into build_optimizer function
     """
     dict_config = {"name": "rms", "rms": {}}
-    opt_get = optimizer.get_optimizer(dict_config)
+    opt_get = optimizer.build_optimizer(dict_config)
     assert isinstance(opt_get, tf.keras.optimizers.RMSprop)
 
 
-def test_get_optimizer_error():
+def test_build_optimizer_error():
     """
     Assert value_error raised if
     unknown optimizer type is passed
-    to get_optimizer func,
+    to build_optimizer func,
     """
     with pytest.raises(ValueError):
         optimizer.build_optimizer({"name": "random"})
