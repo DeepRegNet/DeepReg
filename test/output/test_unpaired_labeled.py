9b794ff9e932580eabc9ecddafeb3a7ed476de92
Yunguan Fu <yunguan.fu.18@ucl.ac.uk>
2020-07-20 22:31:51 +0100

    Issue #170: comment out a redundant test

--- test/output/test_unpaired_labeled.py
+++ test/output/test_unpaired_labeled.py
@@ -6,14 +6,15 @@ from deepreg.util import train_and_predict_with_config
 
 
 def test_unpaired_labeled():
-    train_and_predict_with_config(
-        test_name="unpaired_labeled_nifti",
-        config_path=[
-            "deepreg/config/test/ddf.yaml",
-            "deepreg/config/test/unpaired_nifti.yaml",
-            "deepreg/config/test/labeled.yaml",
-        ],
-    )
+    # comment out the following test as it is included in test_train.py:test_train
+    # train_and_predict_with_config(
+    #     test_name="unpaired_labeled_nifti",
+    #     config_path=[
+    #         "deepreg/config/test/ddf.yaml",
+    #         "deepreg/config/test/unpaired_nifti.yaml",
+    #         "deepreg/config/test/labeled.yaml",
+    #     ],
+    # )
 
     train_and_predict_with_config(
         test_name="unpaired_labeled_h5",
