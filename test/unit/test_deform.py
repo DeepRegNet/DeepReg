1414cc3d0805a264602323160cfef9c72d9c19a8
Qianye Yang <qianye.yang.19@ucl.ac.uk>
2020-07-20 21:27:56 +0100

    unit test for deform.py

--- test/unit/test_deform.py
+++ test/unit/test_deform.py
@@ -20,40 +20,76 @@ def assertTensorsEqual(x, y):
     """
     x = tf.cast(x, dtype=tf.float32)
     y = tf.cast(y, dtype=tf.float32)
+    assert x.shape == y.shape
     return tf.reduce_max(tf.abs(x - y)).numpy() < 1e-6
-
-def assertTensorsShapeEqual(x, y):
-    """assert the tensors have the same shape"""
-    return x.shape == y.shape
 
 def test_gradient_dx():
     """test the calculation of gradient of a 3D images along x-axis"""
-    tensor = np.ones([4, 50, 50, 50])
+    tensor = tf.ones([4, 50, 50, 50])
     get = deform.gradient_dx(tensor)
-    print(get.shape)
+    expect = tf.zeros([4, 48, 48, 48])
+    assert assertTensorsEqual(get, expect)
 
 def test_gradient_dy():
     """test the calculation of gradient of a 3D images along y-axis"""
-    pass
+    tensor = tf.ones([4, 50, 50, 50])
+    get = deform.gradient_dy(tensor)
+    expect = tf.zeros([4, 48, 48, 48])
+    assert assertTensorsEqual(get, expect)
 
 def test_gradient_dz():
     """test the calculation of gradient of a 3D images along z-axis"""
-    pass
+    tensor = tf.ones([4, 50, 50, 50])
+    get = deform.gradient_dz(tensor)
+    expect = tf.zeros([4, 48, 48, 48])
+    assert assertTensorsEqual(get, expect)
 
 def test_gradient_txyz():
     """test the calculation of gradient of a 3D images along xyz-axis"""
-    pass
+    tensor = tf.ones([4, 50, 50, 50, 3])
+    get = deform.gradient_dz(tensor)
+    expect = tf.zeros([4, 48, 48, 48, 3])
+    assert assertTensorsEqual(get, expect)
 
 def test_compute_gradient_l1norm():
     """test the calculation of l1 norm for image gradients"""
-    pass
-
+    tensor = tf.ones([4, 50, 50, 50, 3])
+    get = deform.compute_gradient_norm(tensor, l1=True)
+    expect = tf.zeros([4])
+    assert assertTensorsEqual(get, expect)
+    
 def test_compute_gradient_else():
     """test the calculation of l2 norm for image gradients"""
-    pass
+    tensor = tf.ones([4, 50, 50, 50, 3])
+    get = deform.compute_gradient_norm(tensor)
+    expect = tf.zeros([4])
+    assert assertTensorsEqual(get, expect)
 
 def test_compute_bending_energy():
     """test the calculation of bending energy"""
-    pass
+    tensor = tf.ones([4, 50, 50, 50, 3])
+    get = deform.compute_bending_energy(tensor)
+    expect = tf.zeros([4])
+    assert assertTensorsEqual(get, expect)
 
-test_gradient_dx()
+def test_local_displacement_energy_bending():
+    """compute bending energy for ddf"""
+    tensor = tf.ones([4, 50, 50, 50, 3])
+    get = deform.local_displacement_energy(tensor, 'bending')
+    expect = tf.zeros([4])
+    assert assertTensorsEqual(get, expect)
+
+def test_local_displacement_energy_l1norm():
+    """compute l1 norm for ddf"""
+    tensor = tf.ones([4, 50, 50, 50, 3])
+    get = deform.local_displacement_energy(tensor, 'gradient-l1')
+    expect = tf.zeros([4])
+    assert assertTensorsEqual(get, expect)
+
+def test_local_displacement_energy_l2norm():
+    """compute l2 norm for ddf"""
+    tensor = tf.ones([4, 50, 50, 50, 3])
+    get = deform.local_displacement_energy(tensor, 'gradient-l2')
+    expect = tf.zeros([4])
+    assert assertTensorsEqual(get, expect)
+
