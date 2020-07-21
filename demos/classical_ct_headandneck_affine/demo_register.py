500ab19892afc49143b10693d5f10545aed23b5f
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-07-21 02:45:19 +0100

    Issue #193 initial demo files moved

--- demos/classical_ct_headandneck_affine/demo_register.py
+++ demos/classical_ct_headandneck_affine/demo_register.py
@@ -1,37 +1,41 @@
 """
 Classical affine iterative pairwise registration algorithms as integration tests
 """
-import nibabel
-import tensorflow as tf 
-import deepreg.model.layer as layer
-import deepreg.model.loss.image as image_loss
-import deepreg.model.layer_util as layer_util
 import matplotlib.pyplot as plt
+import nibabel
+import tensorflow as tf
 
+import deepreg.model.layer_util as layer_util
+import deepreg.model.loss.image as image_loss
 
 ## registration parameters
-image_loss_name = 'ssd'
+image_loss_name = "ssd"
 learning_rate = 0.1
 total_iter = int(1000)
 
 
 ## load image
-load_image = lambda fn:tf.cast(tf.expand_dims(nibabel.load(fn).dataobj, axis=0), dtype=tf.float32)
-moving_image = load_image('./data/mr_us/unpaired/train/images/case000000.nii.gz')
+def load_image(fn):
+    return tf.cast(tf.expand_dims(nibabel.load(fn).dataobj, axis=0), dtype=tf.float32)
+
+
+moving_image = load_image("./data/mr_us/unpaired/train/images/case000000.nii.gz")
 # fixed_image = load_image('./data/mr_us/unpaired/train/images/case000001.nii.gz')
 
 # random affine-transformed fixed image
 fixed_image_size = moving_image.shape
 random_transform = layer_util.random_transform_generator(batch_size=1, scale=0.2)
 grid_ref = layer_util.get_reference_grid(grid_size=fixed_image_size[1:4])
-fixed_image = layer_util.resample(vol=moving_image, loc=layer_util.warp_grid(grid_ref, random_transform))
+fixed_image = layer_util.resample(
+    vol=moving_image, loc=layer_util.warp_grid(grid_ref, random_transform)
+)
 
 
 ## optimisation
 @tf.function
 def train_step(grid, weights, optimizer, mov, fix):
     """
-    Train step function for backprop using gradient tape    
+    Train step function for backprop using gradient tape
 
     :param grid: reference grid return from layer_util.get_reference_grid
     :param weights: trainable affine parameters [1, 4, 3]
@@ -47,30 +51,38 @@ def train_step(grid, weights, optimizer, mov, fix):
     optimizer.apply_gradients(zip(gradients, [weights]))
     return loss
 
+
 # affine transformation as trainable weights
-var_affine = tf.Variable(initial_value=[[[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0],[0.0,0.0,0.0]]], trainable=True)
+var_affine = tf.Variable(
+    initial_value=[
+        [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 0.0]]
+    ],
+    trainable=True,
+)
 optimiser = tf.optimizers.Adam(learning_rate)
 for step in range(total_iter):
     loss_opt = train_step(grid_ref, var_affine, optimiser, moving_image, fixed_image)
     if (step % 50) == 0:  # print info
-        tf.print('Step',step, image_loss_name,loss_opt)
+        tf.print("Step", step, image_loss_name, loss_opt)
 
 
 ## predict
-pred_fixed_image = layer_util.resample(vol=moving_image, loc=layer_util.warp_grid(grid_ref, var_affine))
+pred_fixed_image = layer_util.resample(
+    vol=moving_image, loc=layer_util.warp_grid(grid_ref, var_affine)
+)
 
 # display
-idx_slices = [int(5+x*5) for x in range(int(fixed_image_size[3]/5)-1)]
+idx_slices = [int(5 + x * 5) for x in range(int(fixed_image_size[3] / 5) - 1)]
 nIdx = len(idx_slices)
 plt.figure()
 for idx in range(len(idx_slices)):
-    axs = plt.subplot(nIdx, 3, 3*idx+1)
-    axs.imshow(moving_image[0,...,idx_slices[idx]], cmap='gray')
-    axs.axis('off')
-    axs = plt.subplot(nIdx, 3, 3*idx+2)
-    axs.imshow(fixed_image[0,...,idx_slices[idx]], cmap='gray')
-    axs.axis('off')
-    axs = plt.subplot(nIdx, 3, 3*idx+3)
-    axs.imshow(pred_fixed_image[0,...,idx_slices[idx]], cmap='gray')
-    axs.axis('off')
+    axs = plt.subplot(nIdx, 3, 3 * idx + 1)
+    axs.imshow(moving_image[0, ..., idx_slices[idx]], cmap="gray")
+    axs.axis("off")
+    axs = plt.subplot(nIdx, 3, 3 * idx + 2)
+    axs.imshow(fixed_image[0, ..., idx_slices[idx]], cmap="gray")
+    axs.axis("off")
+    axs = plt.subplot(nIdx, 3, 3 * idx + 3)
+    axs.imshow(pred_fixed_image[0, ..., idx_slices[idx]], cmap="gray")
+    axs.axis("off")
 plt.show()
--- demos/classical_mr_prostate_nonrigid/demo_register.py
+++ demos/classical_mr_prostate_nonrigid/demo_register.py
@@ -1,87 +1,109 @@
 """
 Classical nonrigid iterative pairwise registration algorithms as integration tests
 """
-import nibabel
-import tensorflow as tf 
-import deepreg.model.layer as layer
-import deepreg.model.loss.image as image_loss
-import deepreg.model.loss.deform as deform_loss
-import deepreg.model.layer_util as layer_util
 import matplotlib.pyplot as plt
+import nibabel
+import tensorflow as tf
+
+import deepreg.model.layer as layer
+import deepreg.model.layer_util as layer_util
+import deepreg.model.loss.deform as deform_loss
+import deepreg.model.loss.image as image_loss
+
+# https://github.com/YipengHu/example-data/blob/master/hnct/demo.h5
 
 
 ## registration parameters
-image_loss_name = 'ssd'
-deform_loss_name = 'bending'
+image_loss_name = "ssd"
+deform_loss_name = "bending"
 weight_deform_loss = 1e3
 learning_rate = 0.1
 total_iter = int(3000)
 
 
 ## load image
-load_image = lambda fn:tf.cast(tf.expand_dims(nibabel.load(fn).dataobj, axis=0), dtype=tf.float32)
-moving_image = load_image('./data/mr_us/unpaired/train/images/case000000.nii.gz')
+def load_image(fn):
+    return tf.cast(tf.expand_dims(nibabel.load(fn).dataobj, axis=0), dtype=tf.float32)
+
+
+moving_image = load_image("./data/mr_us/unpaired/train/images/case000000.nii.gz")
 # fixed_image = load_image('./data/mr_us/unpaired/train/images/case000001.nii.gz')
 
 # random affine-transformed fixed image
 random_transform = layer_util.random_transform_generator(batch_size=1, scale=0.2)
 grid_ref = layer_util.get_reference_grid(grid_size=moving_image.shape[1:4])
-fixed_image = layer_util.resample(vol=moving_image, loc=layer_util.warp_grid(grid_ref, random_transform))
+fixed_image = layer_util.resample(
+    vol=moving_image, loc=layer_util.warp_grid(grid_ref, random_transform)
+)
 
 
 ## optimisation
 @tf.function
 def train_step(warper, weights, optimizer, mov, fix):
     """
-    Train step function for backprop using gradient tape    
+    Train step function for backprop using gradient tape
 
     :param warper: warping function returned from layer.Warping
     :param weights: trainable ddf [1, f_dim1, f_dim2, f_dim3, 3]
     :param optimizer: tf.optimizers
     :param mov: moving image [1, m_dim1, m_dim2, m_dim3]
     :param fix: fixed image [1, f_dim1, f_dim2, f_dim3]
-    :return: 
+    :return:
         loss: overall loss to optimise
         loss_image: image dissimilarity
         loss_deform: deformation regularisation
     """
     with tf.GradientTape() as tape:
         pred = warper(inputs=[weights, mov])
-        loss_image = image_loss.similarity_fn(y_true=fix, y_pred=pred, name=image_loss_name)
+        loss_image = image_loss.similarity_fn(
+            y_true=fix, y_pred=pred, name=image_loss_name
+        )
         loss_deform = deform_loss.local_displacement_energy(weights, deform_loss_name)
-        loss = loss_image + weight_deform_loss*loss_deform
+        loss = loss_image + weight_deform_loss * loss_deform
     gradients = tape.gradient(loss, [weights])
     optimizer.apply_gradients(zip(gradients, [weights]))
     return loss, loss_image, loss_deform
 
+
 # ddf as trainable weights
 fixed_image_size = fixed_image.shape
 initialiser = tf.random_normal_initializer(mean=0, stddev=1e-3)
-var_ddf = tf.Variable(initialiser(fixed_image_size+[3]), name='ddf', trainable=True)
+var_ddf = tf.Variable(initialiser(fixed_image_size + [3]), name="ddf", trainable=True)
 
 warping = layer.Warping(fixed_image_size=fixed_image_size[1:4])
 optimiser = tf.optimizers.Adam(learning_rate)
 for step in range(total_iter):
-    loss_opt, loss_image_opt, loss_deform_opt = train_step(warping, var_ddf, optimiser, moving_image, fixed_image)
+    loss_opt, loss_image_opt, loss_deform_opt = train_step(
+        warping, var_ddf, optimiser, moving_image, fixed_image
+    )
     if (step % 50) == 0:  # print info
-        tf.print('Step',step, 'loss',loss_opt, image_loss_name,loss_image_opt, deform_loss_name,loss_deform_opt)
+        tf.print(
+            "Step",
+            step,
+            "loss",
+            loss_opt,
+            image_loss_name,
+            loss_image_opt,
+            deform_loss_name,
+            loss_deform_opt,
+        )
 
 
 ## predict
 pred_fixed_image = warping(inputs=[var_ddf, moving_image])
 
 # display
-idx_slices = [int(5+x*5) for x in range(int(fixed_image_size[3]/5)-1)]
+idx_slices = [int(5 + x * 5) for x in range(int(fixed_image_size[3] / 5) - 1)]
 nIdx = len(idx_slices)
 plt.figure()
 for idx in range(len(idx_slices)):
-    axs = plt.subplot(nIdx, 3, 3*idx+1)
-    axs.imshow(moving_image[0,...,idx_slices[idx]], cmap='gray')
-    axs.axis('off')
-    axs = plt.subplot(nIdx, 3, 3*idx+2)
-    axs.imshow(fixed_image[0,...,idx_slices[idx]], cmap='gray')
-    axs.axis('off')
-    axs = plt.subplot(nIdx, 3, 3*idx+3)
-    axs.imshow(pred_fixed_image[0,...,idx_slices[idx]], cmap='gray')
-    axs.axis('off')
+    axs = plt.subplot(nIdx, 3, 3 * idx + 1)
+    axs.imshow(moving_image[0, ..., idx_slices[idx]], cmap="gray")
+    axs.axis("off")
+    axs = plt.subplot(nIdx, 3, 3 * idx + 2)
+    axs.imshow(fixed_image[0, ..., idx_slices[idx]], cmap="gray")
+    axs.axis("off")
+    axs = plt.subplot(nIdx, 3, 3 * idx + 3)
+    axs.imshow(pred_fixed_image[0, ..., idx_slices[idx]], cmap="gray")
+    axs.axis("off")
 plt.show()
