8fd6d7a93431368159df090f2173f561b1bc4fe3
Yipeng Hu <yipenghu@yh-xps-laptop.localdomain>
2020-07-21 23:05:12 +0100

    Issue #193 working affine demo added

--- demos/classical_ct_headandneck_affine/demo_register.py
+++ demos/classical_ct_headandneck_affine/demo_register.py
@@ -7,7 +7,7 @@ import h5py
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-import deepreg.model.layer_util as layer_util
+import deepreg.model.layer_util as util
 import deepreg.model.loss.image as image_loss
 
 current_path = os.getcwd()
@@ -19,7 +19,7 @@ FILE_PATH = os.path.join(DATA_PATH, "demo.h5")
 
 ## registration parameters
 image_loss_name = "ssd"
-learning_rate = 0.1
+learning_rate = 0.01
 total_iter = int(1000)
 
 
@@ -30,17 +30,25 @@ if not os.path.exists(FILE_PATH):
     raise ("Download the data using demo_data.py script")
 
 fid = h5py.File(FILE_PATH, "r")
-fixed_image = fid["image"]
+fixed_image = tf.cast(tf.expand_dims(fid["image"], axis=0), dtype=tf.float32)
+fixed_image = (fixed_image - tf.reduce_min(fixed_image)) / (
+    tf.reduce_max(fixed_image) - tf.reduce_min(fixed_image)
+)  # normalisation to [0,1]
 
-# moving_image = load_image("./data/mr_us/unpaired/train/images/case000000.nii.gz")
-# fixed_image = load_image('./data/mr_us/unpaired/train/images/case000001.nii.gz')
-
-# random affine-transformed fixed image
+# generate a radomly-affine-transformed moving image
 fixed_image_size = fixed_image.shape
-random_transform = layer_util.random_transform_generator(batch_size=1, scale=0.2)
-grid_ref = layer_util.get_reference_grid(grid_size=fixed_image_size[1:4])
-moving_image = layer_util.resample(
-    vol=fixed_image, loc=layer_util.warp_grid(grid_ref, random_transform)
+transform_random = util.random_transform_generator(batch_size=1, scale=0.2)
+grid_ref = util.get_reference_grid(grid_size=fixed_image_size[1:4])
+grid_random = util.warp_grid(grid_ref, transform_random)
+moving_image = util.resample(vol=fixed_image, loc=grid_random)
+# warp the labels to get ground-truth using the same random affine, for validation
+fixed_labels = tf.cast(tf.expand_dims(fid["label"], axis=0), dtype=tf.float32)
+moving_labels = tf.stack(
+    [
+        util.resample(vol=fixed_labels[..., idx], loc=grid_random)
+        for idx in range(fixed_labels.shape[4])
+    ],
+    axis=4,
 )
 
 
@@ -50,7 +58,7 @@ def train_step(grid, weights, optimizer, mov, fix):
     """
     Train step function for backprop using gradient tape
 
-    :param grid: reference grid return from layer_util.get_reference_grid
+    :param grid: reference grid return from util.get_reference_grid
     :param weights: trainable affine parameters [1, 4, 3]
     :param optimizer: tf.optimizers
     :param mov: moving image [1, m_dim1, m_dim2, m_dim3]
@@ -58,8 +66,10 @@ def train_step(grid, weights, optimizer, mov, fix):
     :return loss: image dissimilarity to minimise
     """
     with tf.GradientTape() as tape:
-        pred = layer_util.resample(vol=mov, loc=layer_util.warp_grid(grid, weights))
-        loss = image_loss.similarity_fn(y_true=fix, y_pred=pred, name=image_loss_name)
+        pred = util.resample(vol=mov, loc=util.warp_grid(grid, weights))
+        loss = image_loss.dissimilarity_fn(
+            y_true=fix, y_pred=pred, name=image_loss_name
+        )
     gradients = tape.gradient(loss, [weights])
     optimizer.apply_gradients(zip(gradients, [weights]))
     return loss
@@ -79,11 +89,9 @@ for step in range(total_iter):
         tf.print("Step", step, image_loss_name, loss_opt)
 
 
-## predict
-pred_fixed_image = layer_util.resample(
-    vol=moving_image, loc=layer_util.warp_grid(grid_ref, var_affine)
-)
-
+## warp the moving image using the optimised affine transformation
+grid_opt = util.warp_grid(grid_ref, var_affine)
+warped_moving_image = util.resample(vol=moving_image, loc=grid_opt)
 # display
 idx_slices = [int(5 + x * 5) for x in range(int(fixed_image_size[3] / 5) - 1)]
 nIdx = len(idx_slices)
@@ -96,8 +104,38 @@ for idx in range(len(idx_slices)):
     axs.imshow(fixed_image[0, ..., idx_slices[idx]], cmap="gray")
     axs.axis("off")
     axs = plt.subplot(nIdx, 3, 3 * idx + 3)
-    axs.imshow(pred_fixed_image[0, ..., idx_slices[idx]], cmap="gray")
+    axs.imshow(warped_moving_image[0, ..., idx_slices[idx]], cmap="gray")
     axs.axis("off")
+plt.ion()
 plt.show()
+
+
+## warp the moving labels using the optimised affine transformation
+warped_moving_labels = tf.stack(
+    [
+        util.resample(vol=moving_labels[..., idx], loc=grid_opt)
+        for idx in range(fixed_labels.shape[4])
+    ],
+    axis=4,
+)
+
+# display
+for idx_label in range(fixed_labels.shape[4]):
+    plt.figure()
+    for idx in range(len(idx_slices)):
+        axs = plt.subplot(nIdx, 3, 3 * idx + 1)
+        axs.imshow(moving_labels[0, ..., idx_slices[idx], idx_label], cmap="gray")
+        axs.axis("off")
+        axs = plt.subplot(nIdx, 3, 3 * idx + 2)
+        axs.imshow(fixed_labels[0, ..., idx_slices[idx], idx_label], cmap="gray")
+        axs.axis("off")
+        axs = plt.subplot(nIdx, 3, 3 * idx + 3)
+        axs.imshow(
+            warped_moving_labels[0, ..., idx_slices[idx], idx_label], cmap="gray"
+        )
+        axs.axis("off")
+    plt.ion()
+    plt.show()
+
 
 os.chdir(current_path)
