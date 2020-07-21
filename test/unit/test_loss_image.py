c6b6c01decf8bc04112dada0d9b3106fd99d299c
ZheMin <zhemincuhk@gmail.com>
2020-07-21 23:48:38 +0800

    Issue #107: add the test for dissimilarity_fn in test_loss_image.py

--- test/unit/test_loss_image.py
+++ test/unit/test_loss_image.py
@@ -3,6 +3,7 @@ Tests for deepreg/model/loss/image.py in
 pytest style
 """
 import numpy as np
+import pytest
 import tensorflow as tf
 
 import deepreg.model.loss.image as image
@@ -26,20 +27,26 @@ def test_dissimilarity_fn():
     """
     tensor_true = np.array(range(12)).reshape((2, 1, 2, 3))
     tensor_pred = 0.6 * np.ones((2, 1, 2, 3))
+    tensor_true = tf.convert_to_tensor(tensor_true)
+    tensor_pred = tf.convert_to_tensor(tensor_pred)
+    tensor_true = tf.cast(tensor_true, dtype=tf.float32)
+    tensor_pred = tf.cast(tensor_pred, dtype=tf.float32)
 
-    tensor_true_expand = tf.expand_dims(tensor_true, axis=4)
-    tensor_pred_expand = tf.expand_dims(tensor_pred, axis=4)
-    tensor_true_expand_np = np.array(tensor_true_expand)
-    tensor_pred_expand_np = np.array(tensor_pred_expand)
+    # tensor_true_expand = tf.expand_dims(tensor_true, axis=4)
+    # tensor_pred_expand = tf.expand_dims(tensor_pred, axis=4)
+    # tensor_true_expand_np = np.array(tensor_true_expand)
+    # tensor_pred_expand_np = np.array(tensor_pred_expand)
 
-    get_ncc = image.local_normalized_cross_correlation(
-        tensor_true_expand_np, tensor_pred_expand_np, kernel_size=9
-    )
-    expect_ncc = [0.68002254, 0.9608879]
-    get_ssd = image.ssd(tensor_true_expand_np, tensor_pred_expand_np)
+    name_ncc = "lncc"
+    get_ncc = image.dissimilarity_fn(tensor_true, tensor_pred, name_ncc)
+    expect_ncc = [-0.68002254, -0.9608879]
+    name_ssd = "ssd"
+    get_ssd = image.dissimilarity_fn(tensor_true, tensor_pred, name_ssd)
     expect_ssd = [6.52666667, 65.32666667]
     assert assertTensorsEqual(get_ncc, expect_ncc)
     assert assertTensorsEqual(get_ssd, expect_ssd)
+    with pytest.raises(ValueError):
+        raise ValueError
 
 
 def test_local_normalized_cross_correlation():
