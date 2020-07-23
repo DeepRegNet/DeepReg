ea837701396a68d14617bb61f147c3885e81fe1a
ebonmati <e.bonmati@ucl.ac.uk>
2020-07-23 15:06:26 +0100

    Issue #13: Removing full path in config_path from demo_train

--- demos/unpaired_ct_abdomen/demo_train.py
+++ demos/unpaired_ct_abdomen/demo_train.py
@@ -7,8 +7,8 @@ gpu_allow_growth = False
 ckpt_path = ""
 log_dir = "unpaired_ct_abdomen_log"
 config_path = [
-    r"/home/ebonmati/DeepReg/deepreg/config/test/ddf.yaml",
-    r"/home/ebonmati/DeepReg/demos/unpaired_ct_abdomen/unpaired_ct_abdomen.yaml",
+    r"deepreg/config/test/ddf.yaml",
+    r"demos/unpaired_ct_abdomen/unpaired_ct_abdomen.yaml",
 ]
 train(
     gpu=gpu,
