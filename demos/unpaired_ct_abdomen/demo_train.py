23097b47213e3c6c6608bff4558f85343609cf6d
ebonmati <e.bonmati@ucl.ac.uk>
2020-07-23 15:03:50 +0100

    Issue #13: Added demo_train for training

---
+++ demos/unpaired_ct_abdomen/demo_train.py
@@ -0,0 +1,19 @@
+from deepreg.train import train
+
+# Training
+
+gpu = ""
+gpu_allow_growth = False
+ckpt_path = ""
+log_dir = "unpaired_ct_abdomen_log"
+config_path = [
+    r"/home/ebonmati/DeepReg/deepreg/config/test/ddf.yaml",
+    r"/home/ebonmati/DeepReg/demos/unpaired_ct_abdomen/unpaired_ct_abdomen.yaml",
+]
+train(
+    gpu=gpu,
+    config_path=config_path,
+    gpu_allow_growth=gpu_allow_growth,
+    ckpt_path=ckpt_path,
+    log_dir=log_dir,
+)
