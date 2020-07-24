5751cbce3592dfab8d2a06d5617936420d2c17c4
ebonmati <e.bonmati@ucl.ac.uk>
2020-07-24 13:41:16 +0100

    Issue #13: Removed decompression of .gz and added data normalisation

--- demos/unpaired_ct_abdomen/demo_data.py
+++ demos/unpaired_ct_abdomen/demo_data.py
@@ -1,7 +1,8 @@
 import tarfile
-import gzip
 import os
 import shutil
+import nibabel as nib
+import numpy as np
 
 # 1.- Extract data --> This will create a new folder "Training"
 project_dir = r"demos/unpaired_ct_abdomen"
@@ -16,23 +17,25 @@ tar_file.close
 img_folder_name = os.path.join(data_folder,"Training/img/")
 label_folder_name = os.path.join(data_folder,"Training/label/")
 
-# 2. Extract all images in "Training/img"
+# 2. Normalise and rename all image files
 for gz_image_file in os.listdir(img_folder_name):
-    with gzip.open(os.path.join(img_folder_name, gz_image_file)) as gzip_file: 
-        image_data = gzip_file.read()
-    nii_image_file = open(os.path.join(img_folder_name, gz_image_file[:-3]),"wb")
-    nii_image_file.write(image_data)
-    nii_image_file.close
-    os.remove(os.path.join(img_folder_name, gz_image_file)) #it should check that the file actually exists
+    image_data = np.asarray(nib.load(os.path.join(img_folder_name, gz_image_file)).dataobj, dtype=np.float32)
+    
+    if np.min(image_data) < 0 or np.max(255.0) > 1:
+        image_data = (image_data-np.min(image_data)) * 255/(np.max(image_data)-np.min(image_data))
+        nii_image = nib.Nifti1Image(image_data, affine=None)
+        nib.save(nii_image, os.path.join(img_folder_name, gz_image_file))
+    os.rename(os.path.join(img_folder_name, gz_image_file), os.path.join(img_folder_name, gz_image_file[3:]))
 
-# 3. Extract all labels in "Training/label"
+# 3. Normalise and rename all label files, and separate labels in multiple channels and binary
 for gz_label_file in os.listdir(label_folder_name):
-    with gzip.open(os.path.join(label_folder_name, gz_label_file)) as gzip_file: 
-        label_data = gzip_file.read()
-    nii_label_file = open(os.path.join(label_folder_name, gz_label_file[:-3]),"wb")
-    nii_label_file.write(label_data)
-    nii_label_file.close
-    os.remove(os.path.join(label_folder_name, gz_label_file)) #it should check that the file actually exists
+    label_data = np.asarray(nib.load(os.path.join(label_folder_name, gz_label_file)).dataobj, dtype=np.int)
+    #labels = np.delete(np.unique(label_data),0)
+    masks = np.concatenate([np.expand_dims((label_data == label).astype(np.int), axis=3) for label in range(1,13)], axis=3)
+    nii_labels = nib.Nifti1Image(masks, affine=None)
+    nib.save(nii_labels, os.path.join(label_folder_name, gz_label_file))
+    os.rename(os.path.join(label_folder_name, gz_label_file), os.path.join(label_folder_name, gz_label_file[5:]))
+    print("min = %.7f max = %.7f " % (np.min(masks), np.max(masks)))
 
 # 4.- Divide data in training, validation and testing
 validation_split = 0.15 # 15% of the data for validation
@@ -49,16 +52,19 @@ test_img_files = img_files[0:int(num_cases*test_split)]
 test_label_files = label_files[0:int(num_cases*test_split)]
 print("The following files will be used in testing: ")
 print(test_img_files)
+print(test_label_files)
 
 validation_img_files = img_files[int(num_cases*test_split):int(num_cases*test_split)+int(num_cases*validation_split)]
 validation_label_files = label_files[int(num_cases*test_split):int(num_cases*test_split)+int(num_cases*validation_split)]
 print("The following files will be used in validation: ")
 print(validation_img_files)
+print(validation_label_files)
 
 train_img_files = img_files[int(num_cases*test_split)+int(num_cases*validation_split):]
 train_label_files = label_files[int(num_cases*test_split)+int(num_cases*validation_split):]
 print("The following files will be used in training: ")
 print(train_img_files)
+print(train_label_files)
 
 # 5.- Copy data into train folder
 train_folder = os.path.join(data_folder, "train")
@@ -86,7 +92,7 @@ for nii_file in validation_img_files:
 for label_file in validation_label_files:
     shutil.move(os.path.join(label_folder_name,label_file),os.path.join(valid_folder,"labels",label_file))
 
-# 7.- Copy data into test directory
+# 7.- Copy data into test folder
 test_folder = os.path.join(data_folder, "test")
 
 if os.path.exists(test_folder) is not True:
