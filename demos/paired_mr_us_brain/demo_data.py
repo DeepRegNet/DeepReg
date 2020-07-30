611a81e16f4d5932bcbd750fe838d0d1e2788304
Shaheer U Saeed <shaheersd@gmail.com>
2020-07-30 16:58:46 +0100

    Issue #14: add full data download and process support

--- demos/paired_mr_us_brain/demo_data.py
+++ demos/paired_mr_us_brain/demo_data.py
@@ -1,16 +1,84 @@
 import os
 import shutil
+import zipfile
 
 import nibabel as nib
 import numpy as np
+from tensorflow.keras.utils import get_file
+from tqdm import tqdm
 
 main_path = os.getcwd()
 
 project_dir = os.path.join(main_path, r"demos/paired_mr_us_brain")
 os.chdir(project_dir)
 
+######## ########
 
-os.system("git clone https://github.com/ucl-candi/dataset_respect.git")
+if os.path.exists("dataset_respect") is not True:
+    os.mkdir("dataset_respect")
+    os.mkdir(r"dataset_respect/paired_mr_us_brain")
+url = "https://ns9999k.webs.sigma2.no/10.11582_2020.00025/EASY-RESECT.zip"
+fname = "EASY-RESECT.zip"
+path_to_zip_file = r"dataset_respect"
+get_file(os.path.join(os.getcwd(), path_to_zip_file, fname), url)
+with zipfile.ZipFile(os.path.join(path_to_zip_file, fname), "r") as zip_ref:
+    zip_ref.extractall(os.path.join(path_to_zip_file, "paired_mr_us_brain"))
+path_to_nifti = os.path.join(
+    path_to_zip_file, "paired_mr_us_brain", "EASY-RESECT", "NIFTI"
+)
+all_folders = os.listdir(path_to_nifti)
+for folder in all_folders:
+    source = (path_to_nifti, folder)
+    destination = r"dataset_respect/paired_mr_us_brain"
+    shutil.move(source, destination)
+test_ratio = 0.25
+path_to_data = r"dataset_respect/paired_mr_us_brain"
+cases_list = os.list_dir(path_to_data)
+os.mkdir(os.path.join(path_to_data, "test"))
+os.mkdir(os.path.join(path_to_data, "train"))
+num_test = round(len(cases_list) * test_ratio)
+for folder in cases_list[:num_test]:
+    source = os.path.join(path_to_data, folder)
+    destination = os.path.join(path_to_data, "test")
+    shutil.move(source, destination)
+for folder in cases_list[num_test:]:
+    source = os.path.join(path_to_data, folder)
+    destination = os.path.join(path_to_data, "train")
+    shutil.move(source, destination)
+folders = os.listdir(path_to_data)
+for folder in folders:
+    sub_folders = os.listdir(os.path.join(path_to_data, folder))
+    for sub_folder in tqdm(sub_folders):
+        files = os.listdir(os.path.join(path_to_data, folder, sub_folder))
+        for file in files:
+            if "T1" in file:
+                arr = nib.load(
+                    os.path.join(path_to_data, folder, sub_folder, file)
+                ).get_data()
+                resize_arr = np.resize(arr, (64, 64, 72))
+                img = nib.Nifti1Image(resize_arr, None)
+                nib.save(
+                    img,
+                    os.path.join(
+                        path_to_data,
+                        folder,
+                        sub_folder,
+                        file.split[".nii"][0] + "_resized.nii.gz",
+                    ),
+                )
+            elif "US" in file:
+                img = nib.load(os.path.join(path_to_data, folder, sub_folder, file))
+                nib.save(
+                    img, os.path.join(path_to_data, folder, sub_folder, file + "nii.gz")
+                )
+
+
+######## ########
+
+# os.system("git clone https://github.com/ucl-candi/dataset_respect.git")
+
+######## ########
+
 
 if os.path.exists("dataset_respect/README.md"):
     os.remove("dataset_respect/README.md")
