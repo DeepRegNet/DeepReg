fea4742b2d45686054965d6e004de3ad816557c1
Shaheer U Saeed <shaheersd@gmail.com>
2020-07-30 16:01:23 +0100

    Issue #14: add visualisation for paired_mr_us_brain demo

--- demos/paired_mr_us_brain/demo_predict.py
+++ demos/paired_mr_us_brain/demo_predict.py
@@ -1,5 +1,7 @@
 import os
 
+import matplotlib.pyplot as plt
+
 from deepreg.predict import predict
 
 ######## PREDICTION ########
@@ -23,3 +25,44 @@ predict(
 )
 
 # the numerical metrics are saved in the logs directory specified
+
+######## VISUALISATION ########
+
+# Now lets load in a few samples from the predicitons and plot them
+
+# change the following line to the path to image0 label0
+path_to_image0_label0 = r"logs/learn2reg_t1_paired_train_logs/test"
+path_to_pred_fixed_img = os.path.join(path_to_image0_label0, r"pair_0/moving_image")
+path_to_moving_img = os.path.join(path_to_image0_label0, r"pair_0/pred_fixed_image")
+
+# change inds_to_plot if different images need to be plotted instead
+
+inds_to_plot = [144, 145, 184, 34, 90, 21]
+sub_plot_counter = 1
+
+for ind in inds_to_plot:
+    plt.subplot(6, 2, sub_plot_counter)
+    label = plt.imread(
+        os.path.join(path_to_moving_img, "depth" + str(ind) + "_moving_image.png")
+    )
+    plt.imshow(label)
+    plt.axis("off")
+    if sub_plot_counter == 1:
+        plt.title("Moving Image")
+
+    plt.subplot(6, 2, sub_plot_counter + 1)
+    pred = plt.imread(
+        os.path.join(
+            path_to_pred_fixed_img, "depth" + str(ind) + "_pred_fixed_image.png"
+        )
+    )
+    plt.imshow(pred)
+    plt.axis("off")
+    if sub_plot_counter == 1:
+        plt.title("Warped Fixed Image")
+
+    sub_plot_counter = sub_plot_counter + 2
+
+path_to_vis = r"logs/learn2reg_t1_paired_train_logs/visualisation.png"
+plt.savefig(path_to_vis)
+print("Visualisation saved to:", path_to_vis)
