6f7689f5d6bbd00914b13764c14af82dc0fb18ae
Yunguan Fu <yunguan.fu.18@ucl.ac.uk>
2020-08-04 00:05:13 +0100

    Issue #122: change tests to pytest style

--- test/unit/test_interface.py
+++ test/unit/test_interface.py
@@ -1,4 +1,9 @@
-import unittest
+# coding=utf-8
+
+"""
+Tests for deepreg/dataset/loader/interface.py
+"""
+import pytest
 
 from deepreg.dataset.loader.interface import (
     AbstractPairedDataLoader,
@@ -7,101 +12,93 @@ from deepreg.dataset.loader.interface import (
 )
 
 
-class TestDataLoader(unittest.TestCase):
-    def setUp(self):
-        """
-        check if init method of loader returns any errors when all required
-        arguments given
-        """
-        # test should not pass when wrong type
-        self.testLoader = DataLoader(
-            labeled=True, num_indices=1, sample_label="", seed=1
-        )
+def test_data_loader():
+    """
+    Test the functions in DataLoader
+    """
 
-    def test_NotImplementedError(self):
-        with self.assertRaises(NotImplementedError):
-            self.testLoader.moving_image_shape()
-            self.testLoader.fixed_image_shape()
-            self.testLoader.num_samples()
-            self.testLoader.get_dataset()
-            self.testLoader.get_dataset_and_preprocess(
-                training=True, batch_size=2, repeat=True, shuffle_buffer_num_batch=2
-            )
+    # init
+    # inputs, no error means passed
+    for labeled in [True, False, None]:
+        DataLoader(labeled=labeled, num_indices=1, sample_label="all", seed=0)
+    for sample_label in ["sample", "all", None]:
+        DataLoader(labeled=True, num_indices=1, sample_label=sample_label, seed=0)
+    for num_indices in [1]:
+        DataLoader(labeled=True, num_indices=num_indices, sample_label="sample", seed=0)
+    for seed in [0, None]:
+        DataLoader(labeled=True, num_indices=1, sample_label="sample", seed=seed)
+
+    # not implemented properties / functions
+    data_loader = DataLoader(labeled=True, num_indices=1, sample_label="sample", seed=0)
+    with pytest.raises(NotImplementedError):
+        data_loader.moving_image_shape
+    with pytest.raises(NotImplementedError):
+        data_loader.fixed_image_shape
+    with pytest.raises(NotImplementedError):
+        data_loader.num_samples
+    with pytest.raises(NotImplementedError):
+        data_loader.get_dataset()
+
+    # implemented functions
+    # TODO test get_dataset_and_preprocess
+
+    data_loader.close()
 
 
-class TestAbstractPairedDataLoader(unittest.TestCase):
-    def setUp(self):
-        self.moving_image_shape = (1, 3, 4)
-        self.fixed_image_shape = (1, 6, 8)
-        self.testLoader = AbstractPairedDataLoader(
-            moving_image_shape=self.moving_image_shape,
-            fixed_image_shape=self.fixed_image_shape,
+def test_abstract_paired_data_loader():
+    """
+    Test the functions in AbstractPairedDataLoader
+    """
+    moving_image_shape = (8, 8, 4)
+    fixed_image_shape = (6, 6, 4)
+
+    # test init invalid shape
+    with pytest.raises(ValueError) as err_info:
+        AbstractPairedDataLoader(
+            moving_image_shape=(2, 2),
+            fixed_image_shape=(3, 3),
             labeled=True,
-            sample_label="",
-            seed=1,
+            sample_label="sample",
         )
+    assert "moving_image_shape and fixed_image_shape have to be length of three" in str(
+        err_info.value
+    )
 
-    @unittest.expectedFailure
-    def test_init_fails(self):
-        # invalid input shape
-        self.testLoader2d = AbstractPairedDataLoader(
-            moving_image_shape=(1, 3),
-            fixed_image_shape=(1, 6),
-            labeled=True,
-            sample_label="",
-            seed=1,
+    # test init valid shapes
+    data_loader = AbstractPairedDataLoader(
+        moving_image_shape=moving_image_shape,
+        fixed_image_shape=fixed_image_shape,
+        labeled=True,
+        sample_label="sample",
+    )
+
+    # test properties
+    assert data_loader.num_indices == 2
+    assert data_loader.moving_image_shape == moving_image_shape
+    assert data_loader.fixed_image_shape == fixed_image_shape
+    assert data_loader.num_samples is None
+
+
+def test_abstract_unpaired_data_loader():
+    """
+    Test the functions in AbstractUnpairedDataLoader
+    """
+    image_shape = (8, 8, 4)
+
+    # test init invalid shape
+    with pytest.raises(ValueError) as err_info:
+        AbstractUnpairedDataLoader(
+            image_shape=(2, 2), labeled=True, sample_label="sample"
         )
-        # argument already declared
-        self.testLoader2d = AbstractPairedDataLoader(
-            moving_image_shape=(1, 3, 4),
-            fixed_image_shape=(1, 6, 8),
-            labeled=True,
-            sample_label="",
-            seed=1,
-            num_indices=2,
-        )
+    assert "image_shape has to be length of three" in str(err_info.value)
 
-        # missing argument
-        self.testLoader2d = AbstractPairedDataLoader(
-            moving_image_shape=(1, 3, 4),
-            fixed_image_shape=(1, 3, 4),
-            labeled=True,
-            seed=1,
-        )
+    # test init valid shapes
+    data_loader = AbstractUnpairedDataLoader(
+        image_shape=image_shape, labeled=True, sample_label="sample"
+    )
 
-    def test_property(self):
-        assert self.testLoader.fixed_image_shape == self.fixed_image_shape
-        assert self.testLoader.moving_image_shape == self.moving_image_shape
-        assert self.testLoader.num_samples == self.testLoader.num_images
-
-
-class TestAbstractUnpairedDataLoader(unittest.TestCase):
-    def setUp(self):
-        self.image_shape = (1, 3, 4)
-        self.testLoader = AbstractUnpairedDataLoader(
-            image_shape=self.image_shape, labeled=True, sample_label="", seed=1
-        )
-
-    @unittest.expectedFailure
-    def test_init_fails(self):
-        # invalid input shape
-        self.testLoader2d = AbstractUnpairedDataLoader(
-            image_shape=(1, 3), labeled=True, sample_label="", seed=1
-        )
-        # argument already declared
-        self.testLoader2d = AbstractUnpairedDataLoader(
-            image_shape=(1, 3, 4), labeled=True, sample_label="", seed=1, num_indices=2
-        )
-
-        # missing argument
-        self.testLoader2d = AbstractUnpairedDataLoader(
-            image_shape=(1, 3, 4), labeled=True, seed=1
-        )
-
-    def test_property(self):
-        assert self.testLoader.fixed_image_shape == self.image_shape
-        assert self.testLoader.moving_image_shape == self.image_shape
-        assert self.testLoader.num_samples == self.testLoader._num_samples
-
-
-# why return self.image and return self.sample ????
+    # test properties
+    assert data_loader.num_indices == 3
+    assert data_loader.moving_image_shape == image_shape
+    assert data_loader.fixed_image_shape == image_shape
+    assert data_loader.num_samples is None
