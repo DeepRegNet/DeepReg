7e4ac03150b6b2fd4b9fe7cf7b5febe92fb67a24
RemiDelaunay <remi.delaunay.17@ucl.ac.uk>
2020-08-10 22:28:57 +0100

    issue #122:add unit tests for test_interface.py

--- test/unit/test_interface.py
+++ test/unit/test_interface.py
@@ -3,12 +3,16 @@
 """
 Tests for deepreg/dataset/loader/interface.py
 """
+import numpy as np
 import pytest
 
 from deepreg.dataset.loader.interface import (
     AbstractPairedDataLoader,
     AbstractUnpairedDataLoader,
+    ConcatenatedDataLoader,
     DataLoader,
+    FileLoader,
+    GeneratorDataLoader,
 )
 
 
@@ -102,3 +106,269 @@ def test_abstract_unpaired_data_loader():
     assert data_loader.moving_image_shape == image_shape
     assert data_loader.fixed_image_shape == image_shape
     assert data_loader.num_samples is None
+
+
+def test_generator_data_loader(caplog):
+    """
+    Test the functions in GeneratorDataLoader
+    """
+    generator = GeneratorDataLoader(labeled=True, num_indices=1, sample_label="all")
+
+    # test properties
+    assert generator.loader_moving_image is None
+    assert generator.loader_moving_image is None
+    assert generator.loader_moving_image is None
+    assert generator.loader_moving_image is None
+
+    # not implemented properties / functions
+    with pytest.raises(NotImplementedError):
+        generator.sample_index_generator()
+
+    # implemented functions
+    # test get_Dataset
+    dummy_array = np.random.random(size=(100, 100, 100)).astype(np.float32)
+
+    # mock generator
+    sequence = [
+        dict(
+            moving_image=dummy_array,
+            fixed_image=dummy_array,
+            moving_label=dummy_array,
+            fixed_label=dummy_array,
+            indices=[1],
+        )
+        for i in range(3)
+    ]
+
+    def mock_generator():
+        for el in sequence:
+            yield el
+
+    # inputs, no error means passed
+    generator.data_generator = mock_generator
+    dataset = generator.get_dataset()
+
+    # check output for data generator
+    expected = dict(
+        moving_image=dummy_array,
+        fixed_image=dummy_array,
+        moving_label=dummy_array,
+        fixed_label=dummy_array,
+        indices=[1],
+    )
+    for got in list(dataset.as_numpy_iterator()):
+        assert all(np.array_equal(got[key], expected[key]) for key in expected.keys())
+
+    # test validate_images_and_labels
+    with pytest.raises(ValueError) as err_info:
+        generator.validate_images_and_labels(
+            fixed_image=None,
+            moving_image=dummy_array,
+            moving_label=None,
+            fixed_label=None,
+            image_indices=[1],
+        )
+    assert "moving image and fixed image must not be None" in str(err_info.value)
+    with pytest.raises(ValueError) as err_info:
+        generator.validate_images_and_labels(
+            fixed_image=dummy_array,
+            moving_image=dummy_array,
+            moving_label=dummy_array,
+            fixed_label=None,
+            image_indices=[1],
+        )
+    assert "moving label and fixed label must be both None or non-None" in str(
+        err_info.value
+    )
+    with pytest.raises(ValueError) as err_info:
+        generator.validate_images_and_labels(
+            fixed_image=dummy_array,
+            moving_image=dummy_array + 1.0,
+            moving_label=None,
+            fixed_label=None,
+            image_indices=[1],
+        )
+    assert "Sample [1]'s moving_image's values are not between [0, 1]" in str(
+        err_info.value
+    )
+    with pytest.raises(ValueError) as err_info:
+        generator.validate_images_and_labels(
+            fixed_image=dummy_array,
+            moving_image=np.random.random(size=(100, 100)),
+            moving_label=None,
+            fixed_label=None,
+            image_indices=[1],
+        )
+    assert "Sample [1]'s moving_image' shape should have dimension of 3. " in str(
+        err_info.value
+    )
+    with pytest.raises(ValueError) as err_info:
+        generator.validate_images_and_labels(
+            fixed_image=dummy_array,
+            moving_image=dummy_array,
+            moving_label=np.random.random(size=(100, 100)),
+            fixed_label=dummy_array,
+            image_indices=[1],
+        )
+    assert "Sample [1]'s moving_label' shape should have dimension of 3 or 4. " in str(
+        err_info.value
+    )
+    with pytest.raises(ValueError) as err_info:
+        generator.validate_images_and_labels(
+            fixed_image=dummy_array,
+            moving_image=dummy_array,
+            moving_label=np.random.random(size=(100, 100, 100, 3)),
+            fixed_label=np.random.random(size=(100, 100, 100, 4)),
+            image_indices=[1],
+        )
+    assert (
+        "Sample [1]'s moving image and fixed image have different numbers of labels."
+        in str(err_info.value)
+    )
+
+    # warning
+    generator.validate_images_and_labels(
+        fixed_image=dummy_array,
+        moving_image=dummy_array,
+        moving_label=np.random.random(size=(100, 100, 90)),
+        fixed_label=dummy_array,
+        image_indices=[1],
+    )
+    assert "Sample [1]'s moving image and label have different shapes. " in caplog.text
+    generator.validate_images_and_labels(
+        fixed_image=dummy_array,
+        moving_image=dummy_array,
+        moving_label=dummy_array,
+        fixed_label=np.random.random(size=(100, 100, 90)),
+        image_indices=[1],
+    )
+    assert "Sample [1]'s fixed image and label have different shapes. " in caplog.text
+
+    # test sample_image_label method
+    # for unlabeled input data
+    got = next(
+        generator.sample_image_label(
+            fixed_image=dummy_array,
+            moving_image=dummy_array,
+            moving_label=None,
+            fixed_label=None,
+            image_indices=[1],
+        )
+    )
+    expected = dict(
+        moving_image=dummy_array,
+        fixed_image=dummy_array,
+        indices=np.asarray([1] + [-1], dtype=np.float32),
+    )
+    assert all(np.array_equal(got[key], expected[key]) for key in expected.keys())
+
+    # for data with one label
+    got = next(
+        generator.sample_image_label(
+            fixed_image=dummy_array,
+            moving_image=dummy_array,
+            moving_label=dummy_array,
+            fixed_label=dummy_array,
+            image_indices=[1],
+        )
+    )
+    expected = dict(
+        moving_image=dummy_array,
+        fixed_image=dummy_array,
+        moving_label=dummy_array,
+        fixed_label=dummy_array,
+        indices=np.asarray([1] + [0], dtype=np.float32),
+    )
+    assert all(np.array_equal(got[key], expected[key]) for key in expected.keys())
+
+    # for data with multiple labels
+    dummy_labels = np.random.random(size=(100, 100, 100, 3))
+    got = generator.sample_image_label(
+        fixed_image=dummy_array,
+        moving_image=dummy_array,
+        moving_label=dummy_labels,
+        fixed_label=dummy_labels,
+        image_indices=[1],
+    )
+    for label_index in range(dummy_labels.shape[3]):
+        got_iter = next(got)
+        expected = dict(
+            moving_image=dummy_array,
+            fixed_image=dummy_array,
+            moving_label=dummy_labels[..., label_index],
+            fixed_label=dummy_labels[..., label_index],
+            indices=np.asarray([1] + [label_index], dtype=np.float32),
+        )
+        assert all(
+            np.array_equal(got_iter[key], expected[key]) for key in expected.keys()
+        )
+
+
+def test_concatenated_data_loader():
+    """
+    Test the functions in ConcatenatedDataLoader
+    """
+    image_shape = (8, 8, 4)
+    data_loader = AbstractUnpairedDataLoader(
+        image_shape=image_shape, labeled=True, sample_label="sample", seed=1
+    )
+    data_loader._num_samples = 2
+    data_loaders = [data_loader, data_loader]
+
+    # inputs, no error means passed
+    concatenated_loader = ConcatenatedDataLoader(data_loaders)
+
+    # check init fails with empty list
+    with pytest.raises(AssertionError):
+        ConcatenatedDataLoader(data_loaders=[])
+
+    # not implemented properties / functions
+    with pytest.raises(NotImplementedError):
+        concatenated_loader.get_dataset()
+
+    # implemented property/funtion
+    assert concatenated_loader.moving_image_shape == image_shape
+    assert concatenated_loader.fixed_image_shape == image_shape
+    assert concatenated_loader.num_samples == 2 * len(data_loaders)
+
+    concatenated_loader.close()
+
+
+def test_file_loader():
+    """
+    Test the functions in ConcatenatedDataLoader
+    """
+    loader_grouped = FileLoader(dir_path="", name="", grouped=True)
+    loader_ungrouped = FileLoader(dir_path="", name="", grouped=False)
+    group_sample_dict = {"1": ["1", "2"]}
+    group_ids = sorted(list(group_sample_dict.keys()))
+
+    # not implemented properties / functions
+    with pytest.raises(NotImplementedError):
+        loader_grouped.get_data(1)
+        loader_grouped.get_data_ids()
+        loader_grouped.get_num_images()
+        loader_grouped.set_group_structure()
+        loader_grouped.close()
+
+    # test grouped file loader
+    assert loader_grouped.group_sample_dict is None
+    assert loader_grouped.group_ids is None
+    loader_grouped.group_sample_dict = group_sample_dict
+    loader_grouped.group_ids = group_ids
+    assert loader_grouped.get_num_groups() == len(group_ids)
+    assert loader_grouped.get_num_images_per_group() == [
+        len(group_sample_dict[g]) for g in group_ids
+    ]
+
+    # test ungrouped file loader
+    with pytest.raises(AttributeError):
+        loader_ungrouped.group_sample_dict
+    with pytest.raises(AttributeError):
+        loader_ungrouped.group_ids
+    with pytest.raises(AssertionError):
+        loader_ungrouped.get_num_groups()
+    with pytest.raises(AssertionError):
+        loader_ungrouped.get_num_images_per_group()
+    with pytest.raises(NotImplementedError):
+        loader_ungrouped.close()
