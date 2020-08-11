457de7160eb70e5eadb01cae3c1911d9cdb7c1a8
Shaheer U Saeed <shaheersd@gmail.com>
2020-08-11 08:07:23 +0100

    Issue #250: change default shape in config

--- demos/paired_mr_us_brain/demo_train.py
+++ demos/paired_mr_us_brain/demo_train.py
@@ -2,7 +2,7 @@ from deepreg.train import train
 
 ######## NOW WE DO THE TRAINING ########
 
-gpu = ""
+gpu = "0"
 gpu_allow_growth = False
 ckpt_path = ""
 log_dir = "learn2reg_t1_paired_train_logs"
@@ -10,10 +10,12 @@ config_path = [
     r"deepreg/config/test/ddf.yaml",
     r"demos/paired_mr_us_brain/paired_mr_us_brain.yaml",
 ]
+save_png = True
 train(
     gpu=gpu,
     config_path=config_path,
     gpu_allow_growth=gpu_allow_growth,
     ckpt_path=ckpt_path,
     log_dir=log_dir,
+    save_png=save_png,
 )
