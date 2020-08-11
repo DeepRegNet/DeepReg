39b169c23005fb777cc84e0148bca0b86c3b11ab
Shaheer U Saeed <shaheersd@gmail.com>
2020-08-11 08:08:32 +0100

    Issue #250: fix typo

--- demos/paired_mr_us_brain/demo_train.py
+++ demos/paired_mr_us_brain/demo_train.py
@@ -10,12 +10,10 @@ config_path = [
     r"deepreg/config/test/ddf.yaml",
     r"demos/paired_mr_us_brain/paired_mr_us_brain.yaml",
 ]
-save_png = True
 train(
     gpu=gpu,
     config_path=config_path,
     gpu_allow_growth=gpu_allow_growth,
     ckpt_path=ckpt_path,
     log_dir=log_dir,
-    save_png=save_png,
 )
