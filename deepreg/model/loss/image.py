192287a221886177fea0ee81bbcf0973f9b16d13
Yunguan Fu <yunguan.fu.18@ucl.ac.uk>
2020-08-11 21:04:55 +0100

    Issue #20: make hyper-params changable and simplify the code

--- deepreg/model/loss/image.py
+++ deepreg/model/loss/image.py
@@ -86,49 +86,49 @@ def ssd(y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
     return tf.reduce_mean(tf.square(y_true - y_pred), axis=[1, 2, 3, 4])
 
 
-def global_mutual_information(y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+def global_mutual_information(
+    y_true: tf.Tensor, y_pred: tf.Tensor, num_bins: int = 23, sigma_ratio: float = 0.5
+) -> tf.Tensor:
     """
     differentiable global mutual information loss via Parzen windowing method.
-    reference: https://dspace.mit.edu/handle/1721.1/123142, Section 3.1, equation 3.1-3.5
+    reference: https://dspace.mit.edu/handle/1721.1/123142, Section 3.1, equation 3.1-3.5, Algorithm 1
 
-    :y_true: shape = (batch, dim1, dim2, dim3, ch)
-    :y_pred: shape = (batch, dim1, dim2, dim3, ch)
+    :param y_true: shape = (batch, dim1, dim2, dim3, ch)
+    :param y_pred: shape = (batch, dim1, dim2, dim3, ch)
+    :param num_bins: int, number of bins for intensity
+    :param sigma_ratio: float, a hyper param for gaussian function
     :return: shape = (batch,)
     """
-    bin_centers = tf.linspace(0.0, 1.1, 23)
-    sigma_ratio = 0.5
-    eps = 1.19209e-08
 
+    bin_centers = tf.linspace(0.0, 1.0, num_bins)  # (num_bins,)
+    bin_centers = bin_centers[None, None, ...]  # (1, 1, num_bins)
     sigma = (
         tf.reduce_mean(bin_centers[1:] - bin_centers[:-1]) * sigma_ratio
-    )  # sigma in the Gaussian function (weighting function W)
-    preterm = 1 / (2 * tf.math.square(sigma))
+    )  # scalar, sigma in the Gaussian function (weighting function W)
+    preterm = 1 / (2 * tf.math.square(sigma))  # scalar
 
     batch, w, h, z, c = y_true.shape
-    y_true = tf.reshape(y_true, [batch, w * h * z, 1])  # shape = (batch, nb_voxels, 1)
-    y_pred = tf.reshape(y_pred, [batch, w * h * z, 1])  # shape = (batch, nb_voxels, 1)
-    nb_voxels = y_true.shape[1] * 1.0  # number of voxels
-
-    vbc = bin_centers[None, None, ...]  # shape = (batch, nb_voxels, num_bins)
+    y_true = tf.reshape(y_true, [batch, w * h * z, 1])  # (batch, nb_voxels, 1)
+    y_pred = tf.reshape(y_pred, [batch, w * h * z, 1])  # (batch, nb_voxels, 1)
+    nb_voxels = y_true.shape[1] * 1.0  # w * h * z, number of voxels
 
     # each voxel contributes continuously to a range of histogram bin
-    I_a = tf.math.exp(
-        -preterm * tf.math.square(y_true - vbc)
-    )  # shape = (batch, nb_voxels, num_bins)
-    I_a /= tf.reduce_sum(I_a, -1, keepdims=True)  # shape = (batch, nb_voxels, num_bins)
-    pa = tf.reduce_mean(I_a, axis=1, keepdims=True)  # shape = (batch, 1, num_bins)
-    I_a_permute = tf.transpose(I_a, (0, 2, 1))  # shape = (batch, num_bins, nb_voxels)
+    Ia = tf.math.exp(
+        -preterm * tf.math.square(y_true - bin_centers)
+    )  # (batch, nb_voxels, num_bins)
+    Ia /= tf.reduce_sum(Ia, -1, keepdims=True)  # (batch, nb_voxels, num_bins)
+    Ia = tf.transpose(Ia, (0, 2, 1))  # (batch, num_bins, nb_voxels)
+    pa = tf.reduce_mean(Ia, axis=-1, keepdims=True)  # (batch, num_bins, 1)
 
-    I_b = tf.math.exp(
-        -preterm * tf.math.square(y_pred - vbc)
-    )  # shape = (batch, nb_voxels, num_bins)
-    I_b /= tf.reduce_sum(I_b, -1, keepdims=True)  # shape = (batch, nb_voxels, num_bins)
-    pb = tf.reduce_mean(I_b, axis=1, keepdims=True)  # shape = (batch, 1, num_bins)
+    Ib = tf.math.exp(
+        -preterm * tf.math.square(y_pred - bin_centers)
+    )  # (batch, nb_voxels, num_bins)
+    Ib /= tf.reduce_sum(Ib, -1, keepdims=True)  # (batch, nb_voxels, num_bins)
+    pb = tf.reduce_mean(Ib, axis=1, keepdims=True)  # (batch, 1, num_bins)
 
-    pa = tf.transpose(pa, (0, 2, 1))  # shape = (batch, num_bins, 1)
-    papb = tf.matmul(pa, pb) + eps  # shape = (batch, num_bins, num_bins)
-    pab = tf.matmul(I_a_permute, I_b)  # shape = (batch, num_bins, num_bins)
+    papb = tf.matmul(pa, pb)  # (batch, num_bins, num_bins)
+    pab = tf.matmul(Ia, Ib)  # (batch, num_bins, num_bins)
     pab /= nb_voxels
 
     # MI: sum(P_ab * log(P_ab/P_aP_b + eps))
-    return tf.reduce_sum(pab * tf.math.log(pab / papb + eps), axis=[1, 2])
+    return tf.reduce_sum(pab * tf.math.log((pab + EPS) / (papb + EPS)), axis=[1, 2])
