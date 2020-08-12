e73b02a6cd6dd7bb1429c769eca7ff77b2bf7c20
ebonmati <e.bonmati@ucl.ac.uk>
2020-08-12 16:32:42 +0200

    Issue #13: Updated demo_plot with new logs files organisation

--- demos/unpaired_ct_abdomen/demo_plot.py
+++ demos/unpaired_ct_abdomen/demo_plot.py
@@ -5,36 +5,36 @@ import matplotlib.pyplot as plt
 # Plot results from predictions
 
 log_dir = "unpaired_ct_abdomen_log"
-images_dir = os.path.join("logs", log_dir, "test")
+images_dir = os.path.join("logs", log_dir, "test/pair_0_1")
 
 # Plotting results of cases 1 and 2 (testing)
 
 plt.subplot(2, 3, 1)
-label_1_fixed_image = plt.imread(os.path.join(images_dir,"label1","depth56_fixed_image.png"))
+label_1_fixed_image = plt.imread(os.path.join(images_dir,"fixed_image","depth56_fixed_image.png"))
 plt.imshow(label_1_fixed_image)
 plt.title("Label 1 fixed image")
 plt.axis("off")
 
 plt.subplot(2, 3, 2)
-label_1_fixed_label = plt.imread(os.path.join(images_dir,"label1","depth56_fixed_label.png"))
+label_1_fixed_label = plt.imread(os.path.join(images_dir,"label_1/fixed_label","depth56_fixed_label.png"))
 plt.imshow(label_1_fixed_label)
 plt.title("Label 1 fixed label")
 plt.axis("off")
 
 plt.subplot(2, 3, 3)
-label_1_predicted = plt.imread(os.path.join(images_dir,"label1","depth56_fixed_label_pred.png"))
+label_1_predicted = plt.imread(os.path.join(images_dir,"label_1/pred_fixed_label","depth56_pred_fixed_label.png"))
 plt.imshow(label_1_predicted)
 plt.title("Label 1 prediction")
 plt.axis("off")
 
 plt.subplot(2, 3, 4)
-label_1_moving_image = plt.imread(os.path.join(images_dir,"label1","depth56_moving_image.png"))
+label_1_moving_image = plt.imread(os.path.join(images_dir,"moving_image","depth56_moving_image.png"))
 plt.imshow(label_1_moving_image)
 plt.title("Label 1 moving image")
 plt.axis("off")
 
 plt.subplot(2, 3, 5)
-label_1_moving_label = plt.imread(os.path.join(images_dir,"label1","depth56_moving_label.png"))
+label_1_moving_label = plt.imread(os.path.join(images_dir,"label_1/moving_label","depth56_moving_label.png"))
 plt.imshow(label_1_moving_label)
 plt.title("Label 1 moving label")
 plt.axis("off")
