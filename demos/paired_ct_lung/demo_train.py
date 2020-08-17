10689d19f5dc4b977680195b93b28f1826f23cbd
Shaheer U Saeed <shaheersd@gmail.com>
2020-08-17 20:41:25 +0100

    Issue #299: use separate config files for demos

--- demos/paired_ct_lung/demo_train.py
+++ demos/paired_ct_lung/demo_train.py
@@ -7,7 +7,7 @@ gpu_allow_growth = False
 ckpt_path = ""
 log_dir = "learn2reg_t2_paired_train_logs"
 config_path = [
-    r"deepreg/config/test/ddf.yaml",
+    r"deepreg/config/test/paired_ct_lung_train.yaml",
     r"demos/paired_ct_lung/paired_ct_lung.yaml",
 ]
 train(
