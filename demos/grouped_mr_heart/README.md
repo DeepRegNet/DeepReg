5b0e0196af623373b12d29893462b4e76f7c7b06
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-08-23 01:57:53 +0100

    Issue #308 demo_data done

--- demos/grouped_mr_heart/README.md
+++ demos/grouped_mr_heart/README.md
@@ -1,22 +1,19 @@
-# Pairwise registration for grouped cardiac multisequence MR images
+# Pairwise registration for grouped multi-sequence cardiac MR images
 
-This demo uses DeepReg to demonstrate a number of features:
-
-- For grouped data in h5 files, e.g. "group-1-2" indicates the 2th visit from Subject 1;
-- Use masks as the images for feature-based registration - aligning the prostate gland
-  segmentation in this case - with deep learning;
-- Register intra-patient longitudinal data.
+This demo uses the grouped dataset loader to register intra-subject multi-sequence
+cardiac magnetic resonance (CMR) images.
 
 ## Author
 
-Yipeng Hu (yipeng.hu@ucl.ac.uk)
+DeepReg Development Team (correspondence: yipeng.hu@ucl.ac.uk)
 
 ## Application
 
-Logitudinal registration detects the temporal changes and normalises the spatial
-difference between images aquired at different time-points. For prostate cancer patients
-under active surveillance programmes, quantifying these changes is useful for detecting
-and monitoring potential cancerous regions.
+Computer-assisted management for patients suffering from myocardial infraction (MI)
+often requires quantifying the difference and compariring the multiple sequences, such
+as he late gadolinium enhancement (LGE) CMR sequence MI, the T2-weighted CMR. The
+collectively provide radiological information otherwise unavailable duing clinical
+practice.
 
 ## Instruction
 
@@ -26,18 +23,17 @@ and monitoring potential cancerous regions.
   ultrasound images and the pre-trained model.
 
 ```bash
-python demos/grouped_mask_prostate_longitudinal/demo_data.py
+python demos/grouped_mr_heart/demo_data.py
 ```
 
 - Call `deepreg_train` from command line. The following example uses a single GPU and
   launches the first of the ten runs of a 9-fold cross-validation, as specified in the
-  [`dataset` section](./grouped_mask_prostate_longitudinal_dataset0.yaml) and the
-  [`train` section](./grouped_mask_prostate_longitudinal_train.yaml), which can be
-  specified in
+  [`dataset` section](./grouped_mr_heart_dataset0.yaml) and the
+  [`train` section](./grouped_mr_heart_train.yaml), which can be specified in
   [seperate yaml files](https://deepregnet.github.io/DeepReg/#/tutorial_experiment?id=cross-validation);
 
 ```bash
-deepreg_train --gpu "0" --config_path demos/grouped_mask_prostate_longitudinal/grouped_mask_prostate_longitudinal.yaml --log_dir grouped_mask_prostate_longitudinal
+deepreg_train --gpu "0" --config_path demos/grouped_mr_heart/grouped_mr_heart.yaml --log_dir grouped_mr_heart
 ```
 
 - Call `deepreg_predict` from command line to use the saved ckpt file for testing on the
@@ -46,7 +42,7 @@ deepreg_train --gpu "0" --config_path demos/grouped_mask_prostate_longitudinal/g
   the results will be saves at the created timestamp-named directories under /logs.
 
 ```bash
-deepreg_predict --gpu "" --config_path demos/grouped_mask_prostate_longitudinal/grouped_mask_prostate_longitudinal.yaml --ckpt_path demos/grouped_mask_prostate_longitudinal/dataset/pre-trained/weights-epoch500.ckpt --save_png --mode test
+deepreg_predict --gpu "" --config_path demos/grouped_mr_heart/grouped_mr_heart.yaml --ckpt_path demos/grouped_mr_heart/dataset/pre-trained/weights-epoch500.ckpt --save_png --mode test
 ```
 
 ## Pre-trained Model
@@ -55,15 +51,22 @@ A pre-trained model will be downloaded after running [demo_data.py](./demo_data.
 unzipped at dataset folder under the demo folder. This pre-trained model will be used by
 default with `deepreg_predict`. Run the user-trained model by specify `--ckpt_path` to
 where the ckpt files are save, in this case (specified by `deepreg_train` as above),
-/logs/grouped_mask_prostate_longitudinal/.
+/logs/grouped_mr_heart/.
 
 ## Data
 
-This is a demo without real clinical data due to regulatory restrictions. The MR and
-ultrasound images used are simulated dummy images.
+This demo uses CMR images from 45 patietns, acquired from the
+[MyoPS2020](http://www.sdspeople.fudan.edu.cn/zhuangxiahai/0/MyoPS20/) challenge held in
+conjunction with MICCAI 2020.
 
 ## Tested DeepReg Tag
 
 tbc
 
 ## References
+
+[1] Xiahai Zhuang: Multivariate mixture model for myocardial segmentation combining
+multi-source images. IEEE Transactions on Pattern Analysis and Machine Intelligence (T
+PAMI), vol. 41, no. 12, 2933-2946, Dec 2019. link. [2] Xiahai Zhuang: Multivariate
+mixture model for cardiac segmentation from multi-sequence MRI. International Conference
+on Medical Image Computing and Computer-Assisted Intervention, pp.581-588, 2016.
--- demos/grouped_mr_heart/demo_data.py
+++ demos/grouped_mr_heart/demo_data.py
@@ -3,8 +3,10 @@ import shutil
 import zipfile
 
 import nibabel as nib
-import numpy as np
+from scipy import ndimage
 from tensorflow.keras.utils import get_file
+
+output_pixdim = 1.5
 
 PROJECT_DIR = r"demos/grouped_mr_heart"
 os.chdir(PROJECT_DIR)
@@ -17,99 +19,104 @@ get_file(os.path.abspath(ZIP_PATH), ORIGIN)
 
 zf = zipfile.ZipFile(ZIP_PATH)
 filenames_all = [fn for fn in zf.namelist() if fn.split(".")[-1] == "gz"]
-num_data = int(len(filenames_all) / 2)
+num_data = int(len(filenames_all) / 3)
 # check indices
 filenames_indices = list(
-    set([int(fn.split("/")[-1].split(".")[0]) for fn in filenames_all])
+    set([int(fn.split("/")[-1].split("_")[0]) for fn in filenames_all])
 )
 if len(filenames_indices) is not num_data:
-    raise ("Images and labels are not in pairs.")
+    raise ("Missing data in image groups.")
 
-print("\nAbdominal CT data downloaded with %d image-label pairs.", num_data)
+if os.path.exists(DATA_PATH):
+    shutil.rmtree(DATA_PATH)
+os.mkdir(DATA_PATH)
 
-ratio_val = 0.1
-ratio_test = 0.15
+print(
+    "\nCMR data from %d subjects downloaded, being extracted and resampled..."
+    % num_data
+)
+print("This may take a few minutes...")
+
+# extract into image groups
+images_path = os.path.join(DATA_PATH, "images")
+os.mkdir(images_path)
+
+for filename in filenames_all:
+    # groups, here same as subjects
+    idx, seq_name = filename.split("/")[-1].split("_")
+    idx_group = filenames_indices.index(int(idx))
+    group_path = os.path.join(images_path, "subject" + "%03d" % idx_group)
+    if os.path.exists(group_path) is not True:
+        os.mkdir(group_path)
+
+    # extract image
+    img_path = os.path.join(group_path, seq_name)
+    with zf.open(filename) as sf, open(img_path, "wb") as df:
+        shutil.copyfileobj(sf, df)
+    # pre-processing
+    img = nib.load(img_path)
+    img = nib.Nifti1Image(
+        ndimage.zoom(
+            img.dataobj, [pd / output_pixdim for pd in img.header.get_zooms()]
+        ),
+        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
+    )  # to a generic affine after resampling
+    img.to_filename(img_path)
+
+os.remove(ZIP_PATH)
+
+print("Done")
+
+ratio_val = 0.05
+ratio_test = 0.10
 num_val = int(num_data * ratio_val)
 num_test = int(num_data * ratio_test)
 num_train = num_data - num_val - num_test
 
 print(
-    "Extracting data into %d-%d-%d for train-val-test (%0.2f-%0.2f-%0.2f)..."
+    "Splitting data into %d-%d-%d for train-val-test (%0.2f-%0.2f-%0.2f)..."
     % (num_train, num_val, num_test, 1 - ratio_val - ratio_test, ratio_val, ratio_test)
 )
 
-# extract to respective folders
+# move images to respective folders
 folders = [os.path.join(DATA_PATH, dn) for dn in ["train", "val", "test"]]
-if os.path.exists(DATA_PATH):
-    shutil.rmtree(DATA_PATH)
-os.mkdir(DATA_PATH)
+
 for fn in folders:
     os.mkdir(fn)
     os.mkdir(os.path.join(fn, "images"))
-    os.mkdir(os.path.join(fn, "labels"))
 
-for filename in filenames_all:
-    # images or labels
-    if filename.startswith("datasets_deepreg_demo-myops/dataset/images"):
-        typename = "images"
-    elif filename.startswith("datasets_deepreg_demo-myops/dataset/labels"):
-        typename = "labels"
-    else:
-        continue
-    # train, val or test
-    idx = filenames_indices.index(int(filename.split("/")[-1].split(".")[0]))
+group_names = os.listdir(images_path)
+for group in group_names:
+    idx = group_names.index(group)
     if idx < num_train:  # train
         fidx = 0
     elif idx < (num_train + num_val):  # val
         fidx = 1
     else:  # test
         fidx = 2
-    filename_dst = os.path.join(folders[fidx], typename, filename.split("/")[-1])
-    with zf.open(filename) as sf, open(filename_dst, "wb") as df:
-        shutil.copyfileobj(sf, df)
-    # re-encode the label files - hard-coded using 13 of them regardless exists or not
-    if typename == "labels":
-        img = nib.load(filename_dst)
-        img1 = np.stack([np.asarray(img.dataobj) == i for i in range(1, 14)], axis=3)
-        img1 = nib.Nifti1Image(img1.astype(np.int8), img.affine)
-        img1.to_filename(filename_dst)
+    shutil.move(os.path.join(images_path, group), os.path.join(folders[fidx], "images"))
 
-os.remove(ZIP_PATH)
+os.rmdir(images_path)
 
 print("Done. \n")
 
 # Download the pre-trained models
-"""
-https://github.com/DeepRegNet/deepreg-model-zoo/raw/master/unpaired_ct_abdomen-unsup.zip
-https://github.com/DeepRegNet/deepreg-model-zoo/raw/master/unpaired_ct_abdomen-weakly.zip
-https://github.com/DeepRegNet/deepreg-model-zoo/raw/master/unpaired_ct_abdomen-comb.zip
-will be downloaded to, respectively,
-dataset/pre-trained/unsup
-dataset/pre-trained/weakly
-dataset/pre-trained/comb
 """
 MODEL_PATH = os.path.join(DATA_PATH, "pre-trained")
 if os.path.exists(MODEL_PATH):
     shutil.rmtree(MODEL_PATH)
 os.mkdir(MODEL_PATH)
 
-model_names = ["unsup", "weakly", "comb"]
-for mname in model_names:
-    model_path_single = os.path.join(MODEL_PATH, mname)
-    os.mkdir(model_path_single)
-    zip_path = "unpaired_ct_abdomen-" + mname
-    origin = (
-        "https://github.com/DeepRegNet/deepreg-model-zoo/raw/master/"
-        + zip_path
-        + ".zip"
-    )
-    zip_file = os.path.join(model_path_single, zip_path + ".zip")
-    get_file(os.path.abspath(zip_file), origin)
-    with zipfile.ZipFile(zip_file, "r") as zf:
-        zf.extractall(path=model_path_single)
-    os.remove(zip_file)
+ZIP_PATH = "grouped_mask_prostate_longitudinal-ckpt"
+ORIGIN = "https://github.com/DeepRegNet/deepreg-model-zoo/raw/master/grouped_mr_heart-ckpt.zip"
+
+zip_file = os.path.join(MODEL_PATH, ZIP_PATH + ".zip")
+get_file(os.path.abspath(zip_file), ORIGIN)
+with zipfile.ZipFile(zip_file, "r") as zf:
+    zf.extractall(path=MODEL_PATH)
+os.remove(zip_file)
 
 print(
-    "Pre-trained models are downloaded and unzipped in individual folders at %s."
-    % os.path.abspath(MODEL_PATH)
+    "Pre-trained model is downloaded and unzipped in %s." % os.path.abspath(MODEL_PATH)
 )
+"""
