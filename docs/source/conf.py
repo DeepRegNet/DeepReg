c11fab6825a81512c585bc503655f99dccdfa29d
Yunguan Fu <yunguan.fu.18@ucl.ac.uk>
2020-08-26 00:00:10 +0100

    Issue #325: fix ci error with sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = []  # ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -55,7 +55,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []  # ["_static"]
 
 # tabs
 sphinx_tabs_valid_builders = ["linkcheck"]
