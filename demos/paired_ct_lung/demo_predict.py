feb07b61e45d7a075beacf9b9034419b227fe453
Nina Montana Brown <nina.brown.15@ucl.ac.uk>
2020-08-26 13:19:26 +0200

    Issue #321: modified predict script such that output titles do not overlap [ci skip]

--- demos/paired_ct_lung/demo_predict.py
+++ demos/paired_ct_lung/demo_predict.py
@@ -49,8 +49,9 @@ path_to_moving_label = os.path.join(path_to_test, "pair_0", "label_0", "moving_l
 
 # change inds_to_plot if different images need to be plotted instead
 
-inds_to_plot = [20, 80, 110, 170, 190, 200]
+inds_to_plot = [144, 145, 184, 140, 150, 180]
 sub_plot_counter = 1
+plt.rcParams["figure.figsize"] = [20, 20]
 
 for ind in inds_to_plot:
     plt.subplot(6, 6, sub_plot_counter)
