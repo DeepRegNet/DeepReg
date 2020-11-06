49d07bc18d0a6ceed530b60ae451891ab83b58cf
Nina Montana Brown <nina.brown.15@ucl.ac.uk>
2020-11-06 10:41:03 +0000

    Issue #54: added tests for output of config_generator

--- test/unit/test_config_generator.py
+++ test/unit/test_config_generator.py
@@ -3,7 +3,7 @@
 Tests functions in config/parser.py
 """
 
-# import os
+import os
 
 import pytest
 from testfixtures import TempDirectory
@@ -16,6 +16,17 @@ def test_gen_path_list():
     Unit test checking list is returned from three
     paths
     """
+    with TempDirectory() as d:
+        #  Make new directories
+        list_paths = ["train", "valid", "test"]
+        list_made = [os.path.join(d.path, item) for item in list_paths]
+        for item in list_paths:
+            d.makedir(item)
+        #  Test ValueError raised
+        result = cg.gen_path_list(*list_made)
+        assert all([item == result[i] for i, item in enumerate(list_made)])
+        #  Cleanup directory
+        d.cleanup()
 
 
 #  parametrised test for value errors raised in gen_path_list
@@ -40,6 +51,23 @@ def test_gen_path_list_neg(list_dirs):
         #  Test ValueError raised
         with pytest.raises(ValueError):
             cg.gen_path_list(list_paths[0], list_paths[1], list_paths[2])
+        #  Cleanup directory
+        d.cleanup()
+
+
+def test_gen_path_one():
+    """
+    Unit test checking list is returned from one path
+    """
+    with TempDirectory() as d:
+        #  Make new directories
+        list_paths = ["train", "valid", "test"]
+        list_made = [os.path.join(d.path, item) for item in list_paths]
+        for item in list_paths:
+            d.makedir(item)
+        #  Test ValueError raised
+        result = cg.gen_path_list_from_one_input(d.path)
+        assert all([item == result[i] for i, item in enumerate(list_made)])
         #  Cleanup directory
         d.cleanup()
 
