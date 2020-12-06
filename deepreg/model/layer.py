aa52a0d6431cf29eb3c6f8a29b9c8396f44ef058
Adria <a.casamitjana@ucl.ac.uk>
2020-12-06 09:20:18 +0000

    Issue #372: get rid of commented lines

--- deepreg/model/layer.py
+++ deepreg/model/layer.py
@@ -721,7 +721,9 @@ class BSplines3DTransform(tf.keras.layers.Layer):
      image (2) performs the interpolation and (3) crops the image around the valid values.
 
     :param cp_spacing: int or tuple of three ints specifying the spacing (in pixels) in each dimension.
-                      When a single int is used, the same spacing to all dimensions is used
+                       When a single int is used, the same spacing to all dimensions is used
+    :param output_shape: tuple containing the output shape (batch_size, dim0, dim1, dim2, 3) of the high resolution
+                         deformation fields.
     :param kwargs:
     """
 
@@ -810,73 +812,6 @@ class BSplines3DTransform(tf.keras.layers.Layer):
             strides=self.cp_spacing,
             padding="VALID",
         )
-
-    # def gaussian_filter_3d(self, kernel_sigma):
-    #
-    #     if isinstance(kernel_sigma, list):
-    #         kernel_size = [
-    #             int(np.ceil(ks * 3) + np.mod(np.ceil(ks * 3) + 1, 2))
-    #             for ks in kernel_sigma
-    #         ]
-    #
-    #     else:
-    #         kernel_size = int(
-    #             np.ceil(kernel_sigma * 3) + np.mod(np.ceil(kernel_sigma * 3) + 1, 2)
-    #         )
-    #
-    #     # Create a x, y coordinate grid of shape (kernel_size, kernel_size, 2)
-    #     coord = [np.arange(ks) for ks in kernel_size]
-    #
-    #     YY, XX, ZZ = np.meshgrid(coord[0], coord[1], coord[2], indexing="ij")
-    #     xyz_grid = np.concatenate(
-    #         (YY[np.newaxis], XX[np.newaxis], ZZ[np.newaxis]), axis=0
-    #     )  # 2, y, x
-    #
-    #     mean = np.asarray([(ks - 1) / 2.0 for ks in kernel_size])
-    #     mean = mean.reshape(-1, 1, 1, 1)
-    #     variance = np.asarray([ks ** 2.0 for ks in kernel_sigma])
-    #     variance = variance.reshape(-1, 1, 1, 1)
-    #
-    #     # Calculate the 2-dimensional gaussian kernel which is
-    #     # the product of two gaussian distributions for two different
-    #     # variables (in this case called x and y)
-    #     # 2.506628274631 = sqrt(2 * pi)
-    #
-    #     norm_kernel = 1.0 / (np.sqrt(2 * np.pi) ** 3 + np.prod(kernel_sigma))
-    #     kernel = norm_kernel * np.exp(
-    #         -np.sum((xyz_grid - mean) ** 2.0 / (2 * variance), axis=0)
-    #     )
-    #
-    #     # Make sure sum of values in gaussian kernel equals 1.
-    #     kernel = kernel / np.sum(kernel)
-    #
-    #     # Reshape
-    #     kernel = kernel.reshape(kernel_size[0], kernel_size[1], kernel_size[2])
-    #
-    #     # Total kernel
-    #     total_kernel = np.zeros(tuple(kernel_size) + (3, 3))
-    #     total_kernel[..., 0, 0] = kernel
-    #     total_kernel[..., 1, 1] = kernel
-    #     total_kernel[..., 2, 2] = kernel
-    #
-    #     return tf.convert_to_tensor(total_kernel, dtype=tf.float32)
-    #
-    # def get_control_points(self, field):
-    #     """
-    #     :param field: tf.Tensor of shape=self.input_shape
-    #     :return: interpolated_field: tf.Tensor
-    #     """
-    #     vol_shape = field.shape[1:-1]
-    #     mesh_shape = [
-    #         tf.cast(tf.math.ceil(v / c) + 3, tf.int32)
-    #         for v, c in zip(vol_shape, self.cp_spacing)
-    #     ]
-    #
-    #     field = tf.nn.conv3d(
-    #         field, self.kernel, strides=(1, 1, 1, 1, 1), padding="SAME"
-    #     )
-    #
-    #     return layer_util.resize3d(image=field, size=mesh_shape)
 
     def call(self, inputs, **kwargs):
         """
