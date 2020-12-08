636eff9dfc9b1f1ac74da67367eabf3e01714e51
Zachary Baum <zac_baum@hotmail.com>
2020-12-08 11:36:57 +0000

    Issue #397: Add docstrings

--- deepreg/download.py
+++ deepreg/download.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python3
+# coding=utf-8
+
+"""
+Module to download additional data/resources that
+are not included in the releases via command line
+interface.
+"""
+
 import argparse
 import os
 from io import BytesIO
@@ -8,7 +15,12 @@ from zipfile import ZipFile
 
 def download(dirs, output_dir="./"):
     """
-    Downloads the files and directories from DeepReg into `output_dir`, keeping only `dirs`.
+    Downloads the files and directories from DeepReg into
+    `output_dir`, keeping only `dirs`.
+
+    :param dirs: the list of directories to save
+    :param output_dir: directory which we use as the root to save output
+    :return: void
     """
 
     output_dir = os.path.abspath(output_dir)  # Get the output directory.
@@ -45,6 +57,9 @@ def download(dirs, output_dir="./"):
 
 
 def main():
+    """
+    Function to run in command line with argparse to download data.
+    """
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
