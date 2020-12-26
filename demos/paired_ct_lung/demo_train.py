c393701b65f1af62ede12306c0f26bdff0e585d4
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2020-12-26 20:15:52 +0000

    Issue #575 gpu config changed back

--- demos/paired_ct_lung/demo_train.py
+++ demos/paired_ct_lung/demo_train.py
@@ -44,7 +44,7 @@ if args.test:
     config_path.append("config/test/demo_paired.yaml")
 
 train(
-    gpu="2",
+    gpu="0",
     config_path=config_path,
     gpu_allow_growth=False,
     ckpt_path="",
