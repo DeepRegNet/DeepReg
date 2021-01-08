fda64211b59859ef8821ac61a56f7e0aa750017b
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2021-01-08 23:58:38 +0000

    Issue #536 adding temp global scripts

---
+++ demos/paired_ct_lung/demo_predict_global.py
@@ -0,0 +1,60 @@
+# pylint: disable=line-too-long
+import argparse
+from datetime import datetime
+
+from deepreg.predict import predict
+
+name = "paired_ct_lung"
+
+# parser is used to simplify testing, by default it is not used
+# please run the script with --no-test flag to ensure non-testing mode
+# for instance:
+# python script.py --no-test
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--test",
+    help="Execute the script for test purpose",
+    dest="test",
+    action="store_true",
+)
+parser.add_argument(
+    "--no-test",
+    help="Execute the script for non-test purpose",
+    dest="test",
+    action="store_false",
+)
+parser.set_defaults(test=False)
+args = parser.parse_args()
+
+print(
+    "\n\n\n\n\n"
+    "=========================================================\n"
+    "The prediction can also be launched using the following command.\n"
+    "deepreg_predict --gpu '' "
+    f"--config_path demos/{name}/{name}.yaml "
+    f"--ckpt_path demos/{name}/dataset/pretrained/ckpt-2000 "
+    f"--log_root demos/{name} "
+    "--log_dir logs_predict "
+    "--save_png --mode test\n"
+    "=========================================================\n"
+    "\n\n\n\n\n"
+)
+
+log_root = f"demos/{name}"
+log_dir = "logs_predict/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+ckpt_path = f"{log_root}/dataset/pretrained/ckpt-2000"
+config_path = [f"{log_root}/{name}_global.yaml"]
+if args.test:
+    config_path.append("config/test/demo_paired.yaml")
+
+predict(
+    gpu="0",
+    gpu_allow_growth=False,
+    ckpt_path=ckpt_path,
+    mode="test",
+    batch_size=1,
+    log_root=log_root,
+    log_dir=log_dir,
+    sample_label="all",
+    config_path=config_path,
+)
---
+++ demos/paired_ct_lung/demo_train_global.py
@@ -0,0 +1,53 @@
+import argparse
+from datetime import datetime
+
+from deepreg.train import train
+
+name = "paired_ct_lung"
+
+# parser is used to simplify testing
+# please run the script with --no-test flag to ensure non-testing mode
+# for instance:
+# python script.py --no-test
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--test",
+    help="Execute the script for test purpose",
+    dest="test",
+    action="store_true",
+)
+parser.add_argument(
+    "--no-test",
+    help="Execute the script for non-test purpose",
+    dest="test",
+    action="store_false",
+)
+parser.set_defaults(test=True)
+args = parser.parse_args()
+
+print(
+    "\n\n\n\n\n"
+    "=======================================================\n"
+    "The training can also be launched using the following command.\n"
+    "deepreg_train --gpu '0' "
+    f"--config_path demos/{name}/{name}.yaml "
+    f"--log_root demos/{name} "
+    "--log_dir logs_train\n"
+    "=======================================================\n"
+    "\n\n\n\n\n"
+)
+
+log_root = f"demos/{name}"
+log_dir = "logs_train/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+config_path = [f"demos/{name}/{name}_global.yaml"]
+if args.test:
+    config_path.append("config/test/demo_paired.yaml")
+
+train(
+    gpu="1",
+    config_path=config_path,
+    gpu_allow_growth=False,
+    ckpt_path="",
+    log_root=log_root,
+    log_dir=log_dir,
+)
