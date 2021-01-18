41033b322c903ebbdaa1168de5a087de26d3b3a3
Shaheer U Saeed <shaheersd@gmail.com>
2021-01-18 16:24:26 +0500

    Issue #629: fix flake8 error

--- demos/update_vis.py
+++ demos/update_vis.py
@@ -40,23 +40,41 @@ def execute_commands(cmds):
                 f"Command {cmd} return with err {e.returncode} {e.output}"
             )
 
-single_config_names = ["grouped_mask_prostate_longitudinal", "grouped_mr_heart","paired_ct_lung", "paired_mrus_brain", "paired_mrus_prostate", "unpaired_ct_lung", "unpaired_mr_brain", "unpaired_us_prostate_cv"]
-slices = ['10,16,20', '14,10,20', '64,50,72', '190,128,96', '12,20,36', '40,48,56', '20,32,44', '50,65,35']
+
+single_config_names = [
+    "grouped_mask_prostate_longitudinal",
+    "grouped_mr_heart",
+    "paired_ct_lung",
+    "paired_mrus_brain",
+    "paired_mrus_prostate",
+    "unpaired_ct_lung",
+    "unpaired_mr_brain",
+    "unpaired_us_prostate_cv",
+]
+slices = [
+    "10,16,20",
+    "14,10,20",
+    "64,50,72",
+    "190,128,96",
+    "12,20,36",
+    "40,48,56",
+    "20,32,44",
+    "50,65,35",
+]
 for name, slice in zip(single_config_names, slices):
-    execute_commands([f'python demos/{name}/demo_data.py'])
-    execute_commands([f'python demos/{name}/demo_predict.py'])
+    execute_commands([f"python demos/{name}/demo_data.py"])
+    execute_commands([f"python demos/{name}/demo_predict.py"])
     check_vis_single_config_demo(name, slice)
 
-for name, slice in zip(["unpaired_ct_abdomen"], ['35,50,65']):
-    execute_commands([f'python demos/{name}/demo_data.py'])
-    execute_commands([f'python demos/unpaired_ct_abdomen/demo_predict.py --method comb'])
+for name, slice in zip(["unpaired_ct_abdomen"], ["35,50,65"]):
+    execute_commands([f"python demos/{name}/demo_data.py"])
+    execute_commands([f"python demos/{name}/demo_predict.py --method comb"])
     check_vis_unpaired_ct_abdomen(name, "comb", slice)
 
-for name, slice in zip(["classical_ct_headneck_affine", "classical_mr_prostate_nonrigid"], ['4,8,12', '4,8,12']):
-    execute_commands([f'python demos/{name}/demo_data.py'])
-    execute_commands([f'python demos/{name}/demo_register.py'])
+for name, slice in zip(
+    ["classical_ct_headneck_affine", "classical_mr_prostate_nonrigid"],
+    ["4,8,12", "4,8,12"],
+):
+    execute_commands([f"python demos/{name}/demo_data.py"])
+    execute_commands([f"python demos/{name}/demo_register.py"])
     check_vis_classical_demo(name, slice)
-
-
-
-
