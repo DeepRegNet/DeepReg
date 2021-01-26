9651d1589666cb92d52dfc4f08e7379a0c0e7e56
Adria <a.casamitjana@ucl.ac.uk>
2021-01-26 11:49:51 +0000

    Issue #604: correct linting errors

--- test/unit/test_loss_util.py
+++ test/unit/test_loss_util.py
@@ -23,16 +23,12 @@ from deepreg.loss.util import (
 
 
 @pytest.mark.parametrize("sigma", [1, 3, 2.2])
-def test_gaussian_kernel1d_sigma(sigma):
-    tail = int(sigma * 3)
-    expected = [np.exp(-0.5 * x ** 2 / sigma ** 2) for x in range(-tail, tail + 1)]
-    expected = expected / np.sum(expected)
-    got = gaussian_kernel1d_sigma(sigma)
-    assert is_equal_tf(got, expected)
-
-
-@pytest.mark.parametrize("sigma", [1, 3, 2.2])
 def test_cauchy_kernel1d(sigma):
+    """
+    Testing the 1-D cauchy kernel
+    :param kernel_size: float
+    :return:
+    """
     tail = int(sigma * 5)
     expected = [1 / ((x / sigma) ** 2 + 1) for x in range(-tail, tail + 1)]
     expected = expected / np.sum(expected)
@@ -40,8 +36,27 @@ def test_cauchy_kernel1d(sigma):
     assert is_equal_tf(got, expected)
 
 
+@pytest.mark.parametrize("sigma", [1, 3, 2.2])
+def test_gaussian_kernel1d_sigma(sigma):
+    """
+    Testing the 1-D gaussian kernel given sigma as input
+    :param kernel_size: float
+    :return:
+    """
+    tail = int(sigma * 3)
+    expected = [np.exp(-0.5 * x ** 2 / sigma ** 2) for x in range(-tail, tail + 1)]
+    expected = expected / np.sum(expected)
+    got = gaussian_kernel1d_sigma(sigma)
+    assert is_equal_tf(got, expected)
+
+
 @pytest.mark.parametrize("kernel_size", [3, 7, 11])
 def test_gaussian_kernel1d_size(kernel_size):
+    """
+    Testing the 1-D gaussian kernel given size as input
+    :param kernel_size: int
+    :return:
+    """
     mean = (kernel_size - 1) / 2.0
     sigma = kernel_size / 3
 
@@ -54,6 +69,11 @@ def test_gaussian_kernel1d_size(kernel_size):
 
 @pytest.mark.parametrize("kernel_size", [3, 7, 11])
 def test_rectangular_kernel1d(kernel_size):
+    """
+    Testing the 1-D rectangular kernel
+    :param kernel_size: int
+    :return:
+    """
     expected = tf.ones(shape=(kernel_size,), dtype=tf.float32)
     got = rectangular_kernel1d(kernel_size)
     assert is_equal_tf(got, expected)
@@ -61,6 +81,11 @@ def test_rectangular_kernel1d(kernel_size):
 
 @pytest.mark.parametrize("kernel_size", [3, 7, 11])
 def test_triangular_kernel1d(kernel_size):
+    """
+    Testing the 1-D triangular kernel
+    :param kernel_size: int (odd number)
+    :return:
+    """
     expected = np.zeros(shape=(kernel_size,), dtype=np.float32)
     expected[kernel_size // 2] = kernel_size // 2 + 1
     for it_k in range(kernel_size // 2):
@@ -110,6 +135,11 @@ def test_negative_loss_mixin(y_true, y_pred, expected):
     Testing NegativeLossMixin class that
     inverts the sign of any value
     returned by a function
+
+    :param y_true: int
+    :param y_pred: int
+    :param expected: int
+    :return:
     """
 
     y_true = tf.constant(y_true, dtype=tf.float32)
