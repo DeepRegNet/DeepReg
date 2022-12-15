f5f327436f1bd8f26d7a34a0f62bb6fb41893956
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-15 01:45:39 +0000

    Adding tarfile member sanitization to extractall()

--- demos/unpaired_mr_brain/demo_data.py
+++ demos/unpaired_mr_brain/demo_data.py
@@ -49,7 +49,26 @@ if exists(path_to_data_folder) is not True:
     makedirs(path_to_data_folder)
 
 with tarfile.open(join(main_path, project_dir, TAR_FILE), "r") as tar_ref:
-    tar_ref.extractall(data_folder_name)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar_ref, data_folder_name)
 
 remove(TAR_FILE)
 print("Files unzipped successfully")
